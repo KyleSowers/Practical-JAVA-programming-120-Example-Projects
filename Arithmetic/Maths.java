811c8f0d8a848838f3c23f22d70470720ba0cb5c
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-05 13:16:27 -0600

    Printed to console each result into a string output.

--- Arithmetic/Maths.java
+++ Arithmetic/Maths.java
@@ -1,5 +1,7 @@
 package Arithmetic;
 
+import java.sql.SQLOutput;
+
 public class Maths {
     public static void main(String args[]) {
         int num1 = 5, num2 = 7, Result1, Result2, Result4;
@@ -8,5 +10,10 @@ public class Maths {
         Result2 = num1 * num2;
         float Result3 = num2 % num1;
         Result4 = num2 - num1;
+
+        System.out.println("Addition of Result is " + Result1);
+        System.out.println("Multiplication of Result is " + Result2);
+        System.out.println("Division of Result is " + Result3);
+        System.out.println("Subtraction of Result is " + Result4);
     }
 }
