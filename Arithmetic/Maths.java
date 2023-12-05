0d0d918ba9e57cbd7756b684d6340c56fe1c5f10
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-05 12:10:20 -0600

    Added Result1,2 & 4 as int variables with equation values stored from use of num1 and num2 integer variables. Result 3 is defined as float and uses num variables through equation for value.

--- Arithmetic/Maths.java
+++ Arithmetic/Maths.java
@@ -3,5 +3,10 @@ package Arithmetic;
 public class Maths {
     public static void main(String args[]) {
         int num1 = 5, num2 = 7, Result1, Result2, Result4;
+
+        Result1 = num1 + num2;
+        Result2 = num1 * num2;
+        float Result3 = num2 % num1;
+        Result4 = num2 - num1;
     }
 }
