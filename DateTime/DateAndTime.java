279fe1fda8f550d9a5803af539ebe3c6f16cd33b
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-05 17:43:36 -0600

    Set main, defined ints day/month/year/second/minute/hour, established and imported the Gregorian Calander.

--- DateTime/DateAndTime.java
+++ DateTime/DateAndTime.java
@@ -1,4 +1,12 @@
 package DateTime;
 
+import java.util.GregorianCalendar;
+
 public class DateAndTime {
+    public static void main(String args[]) {
+        int day, month, year;
+        int second, minute, hour;
+
+        GregorianCalendar date = new GregorianCalendar();
+    }
 }
