83890a16c5aad8a9a7d7808e5c734f8d1117128c
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-06 07:03:02 -0600

    Output to user, a request for 2 integers as an input. Read two imputs that store into varaibles x and y. Called upon the original class Overloading with the sum method we had established. Output the result of the sum to the user.

--- MethodOverloading/Overloading.java
+++ MethodOverloading/Overloading.java
@@ -16,5 +16,12 @@ public class Overloading {
         int x, y;
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Please, enter two integers separately: ");
+        x = Integer.parseInt(br.readLine());
+        y = Integer.parseInt(br.readLine());
+
+        Overloading ol = new Overloading();
+        System.out.println("Result of your numbers are : " + ol.sum(x, y));
     }
     }
