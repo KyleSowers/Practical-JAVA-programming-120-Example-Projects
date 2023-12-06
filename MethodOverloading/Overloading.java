c8b1f4783030a300988f482da0fecba6f0ba964f
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-06 07:07:56 -0600

    Created comments/notes defining the portions of code and their actions in the exercise.

--- MethodOverloading/Overloading.java
+++ MethodOverloading/Overloading.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Overloading {
+    //Creating a method for two integers and the sum of..
     int sum(int num1, int num2) {
         return(num1 * num2);
     }
@@ -13,15 +14,18 @@ public class Overloading {
 
     class MethodCall {
     public static void main(String args[]) throws NumberFormatException, IOException {
+        //Define variables
         int x, y;
-
+        //Establish new BufferReader to read user input
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        //Ask user for integers
         System.out.println("Please, enter two integers separately: ");
+        //Store user results into x and y defined variables
         x = Integer.parseInt(br.readLine());
         y = Integer.parseInt(br.readLine());
-
+        //Call on the Overloading class, creating new instance of
         Overloading ol = new Overloading();
+        //Returning Result statement including use of Overloading sum method of stored x and y variables
         System.out.println("Result of your numbers are : " + ol.sum(x, y));
     }
     }
