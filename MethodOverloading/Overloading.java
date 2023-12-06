005cfe0344cee4a43ca46ec4d6db619152064640
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-06 06:56:28 -0600

    Previous commit left out that num1 and num 2 where a part of the sum method that returned the multiplication of both. Added a class MethodCall that has psvMain, defines variables x and y, and added a new bufferreader "br".

--- MethodOverloading/Overloading.java
+++ MethodOverloading/Overloading.java
@@ -1,8 +1,20 @@
 //PRACTICE 7: A PROGRAM FOR METHOD OVERLOADING
 package MethodOverloading;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Overloading {
     int sum(int num1, int num2) {
         return(num1 * num2);
     }
 }
+
+    class MethodCall {
+    public static void main(String args[]) throws NumberFormatException, IOException {
+        int x, y;
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+    }
