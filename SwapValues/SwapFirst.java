4d0611d079250442e08a9b952a9973360ca7475c
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-05 22:55:44 -0600

    Defined int nums 1 & 2 and values a & b. Created constructor for SwapFirst.

--- SwapValues/SwapFirst.java
+++ SwapValues/SwapFirst.java
@@ -1,4 +1,12 @@
+//Practice 6: A program for swapping two different values
 package SwapValues;
 
 public class SwapFirst {
+    int num1, num2, a, b;
+    SwapFirst(int a, int b) {
+        num1 = a;
+        num2 = b;
+    }
 }
+
+
