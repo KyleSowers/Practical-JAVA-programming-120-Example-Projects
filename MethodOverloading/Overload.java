5566fbb65b262485cbba9d869906d2fe7f7b25b0
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-06 21:43:57 -0600

    Created line notes for the working parts of the code.

--- MethodOverloading/Overload.java
+++ MethodOverloading/Overload.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Overload {
-
+    //Creating method for addition of strings
     String add(String a, String b) {
         return (a + b);
     }
@@ -13,14 +13,18 @@ public class Overload {
 
 class StringOverload {
     public static void main(String args[]) throws IOException {
+        //Adding buffered reader to read user input
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        //define two strings
         String x, y;
+        //Ask user for values
         System.out.println("Please Enter two values: ");
+        //Store values into Strings
         x = br.readLine();
         y = br.readLine();
-
+        //Create new instance of Overload
         Overload ol = new Overload();
+        //Return result of called method of Overload (add) to user
         System.out.println("Result of your two values are: " + ol.add(x, y));
     }
 }
