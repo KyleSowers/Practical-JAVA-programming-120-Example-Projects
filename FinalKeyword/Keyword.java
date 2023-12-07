c7eb553da6036a1e3f85c2049ecc014bda7f1fbb
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-07 12:35:23 -0600

    Added comments for readability of actions taking place.

--- FinalKeyword/Keyword.java
+++ FinalKeyword/Keyword.java
@@ -1,6 +1,8 @@
+//PRACTICE 11: A PROGRAM FOR USE OF FINAL KEYWORD AS A METHOD
 package FinalKeyword;
 
 public class Keyword {
+    //CREATE METHOD "DISPLAY", THAT IS FINAL AND VOIDS RETURN VALUE
     final void display() {
         System.out.println("This is a final Method");
     }
@@ -8,6 +10,7 @@ public class Keyword {
 
 class Hello extends Keyword {
     public static void main(String args[]) {
+        //CREATE NEW INSTANCE OF HELLO (NEW OBJECT), THAT IS THEN CALLED USING CLASS KEYWORDS DISPLAY METHOD
         Hello h1 = new Hello();
         h1.display();
     }
