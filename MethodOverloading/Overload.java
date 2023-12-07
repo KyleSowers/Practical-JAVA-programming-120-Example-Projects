7c2c09d8a1f6944ae122d8077d60d68767781232
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-06 21:40:39 -0600

    Defined x and y strings. Asked user to input two numbers. stroed user values into x and y. Created new instance of class overload. responded to user with result to include the reasult of call on Overload method add, adding the values.

--- MethodOverloading/Overload.java
+++ MethodOverloading/Overload.java
@@ -14,6 +14,14 @@ public class Overload {
 class StringOverload {
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String x, y;
+        System.out.println("Please Enter two values: ");
+        x = br.readLine();
+        y = br.readLine();
+
+        Overload ol = new Overload();
+        System.out.println("Result of your two values are: " + ol.add(x, y));
     }
 }
 
