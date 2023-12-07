e7d8ee540755d12fa8b3fa6e32b300beb51a5dfc
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-07 06:40:29 -0600

    Added a void method to output employee number in a string statement.

--- FinalKeyword/Employee.java
+++ FinalKeyword/Employee.java
@@ -1,4 +1,14 @@
 package FinalKeyword;
 
 public class Employee {
+
+    final int Reg_no;
+    Employee(int num) {
+        Reg_no = num;
+    }
+    void display() {
+        System.out.println("Employee number is: " + Reg_no);
+    }
+
+
 }
