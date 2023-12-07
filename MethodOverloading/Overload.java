476b0ded2f08d349b49c26e85caf89cc342c1d73
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-06 21:34:59 -0600

    Added class StringOvelrload with psvMain. Added a bufferedreader to read inputstream.

--- MethodOverloading/Overload.java
+++ MethodOverloading/Overload.java
@@ -1,5 +1,9 @@
 package MethodOverloading;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Overload {
 
     String add(String a, String b) {
@@ -7,4 +11,9 @@ public class Overload {
     }
 }
 
+class StringOverload {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
 
