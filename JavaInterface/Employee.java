38f925159e4c176b41bb23857ab1e7c63783139b
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-07 21:55:37 -0600

    created a new bufferreader for reading and taking in input.

--- JavaInterface/Employee.java
+++ JavaInterface/Employee.java
@@ -1,7 +1,12 @@
 //PRACTICE 14: A PROGRAM FOR INTERFACE
 package JavaInterface;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Employee {
     int Reg_no, Rt1, Rt2, Rt3;
     String Name;
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 }
