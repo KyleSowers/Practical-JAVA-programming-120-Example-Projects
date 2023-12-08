da97d1c278b4f72c9ac7d7007debc7b4367f0a97
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-08 08:45:17 -0600

    Created line comments to explain code.

--- JavaInterface/Employee.java
+++ JavaInterface/Employee.java
@@ -5,11 +5,12 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Employee {
+    //create instance variables
     int Reg_no, Rt1, Rt2, Rt3;
     String Name;
-
+    //create new buffer reader
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+    //create void method, outputs input request an stores accepted values into instance variables
     void GetInput() throws  Exception {
         System.out.println("Please enter Employee details: Reg_no, Name, Skill Levels of Java, Python, PHP");
         Reg_no = Integer.parseInt(br.readLine());
@@ -18,6 +19,7 @@ public class Employee {
         Rt2 = Integer.parseInt(br.readLine());
         Rt3 = Integer.parseInt(br.readLine());
     }
+    //create method for output statements for each stored input value
     void display() {
         System.out.println("Your Reg_no is: " + Reg_no);
         System.out.println("Your Name is: " + Name);
@@ -28,15 +30,16 @@ public class Employee {
 }
 
 class Output extends Employee {
+    //new instance variables for new class
     int Total;
     float Average;
-
+    //new method, where new instance variables will store desired result of previous instance variables through equation
     void evaluate() throws Exception {
         GetInput();
         Total = Rt1+Rt2+Rt3;
         Average = (float)Total/3;
     }
-
+    //method to ouput results to user of new defined values of this classes instance variables
     void screenshow() {
         System.out.println("Overall Rating is: " + Total);
         System.out.println("Your percentage is: " + Average);
@@ -45,7 +48,9 @@ class Output extends Employee {
 
 class Company {
     public static void main(String args[]) throws Exception {
+        //create new instance of Output class (new Output object)
         Output ot = new Output();
+        //with new Output instance, call upon the three methods that were created for output result
         ot.evaluate();
         ot.screenshow();
         ot.display();
