ce2c9deeb783d61eeab23e576dd09b8830e2e787
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-07 21:49:43 -0600

    Created line comments explaining exercise code.

--- StaticVariable/Employee.java
+++ StaticVariable/Employee.java
@@ -2,21 +2,25 @@
 package StaticVariable;
 
 public class Employee {
+    //Create instance variables
     int Reg_no;
     String Name;
+    //Static variables are assigned to all Employee instances with the associated value
     static String Company = "Gstevewall";
-
+    //create constructor Employee, intakes int and string values, stored into the instance variables
     Employee(int a, String b) {
         Reg_no = a;
         Name = b;
     }
-
+    //create void method, for outputting a message
     void display() {
         System.out.println("Your details are: " + Reg_no + " " + Name + " " + Company);
     }
 
     public static void  main(String args[]) {
+        //create a new employee instance with stored values
         Employee em = new Employee(201, "Ejike");
+        //call upon employee instance with method for output
         em.display();
     }
 }
