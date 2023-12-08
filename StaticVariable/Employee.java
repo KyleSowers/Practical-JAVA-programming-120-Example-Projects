660d2729d674ad60da59de8023ae3e82ac704c3b
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-07 21:40:35 -0600

    Added instance variables of int and string, then a static string instance variable. Created Employee constructor that takes in a value for int and String and stores those values into variables Reg_no and name.

--- StaticVariable/Employee.java
+++ StaticVariable/Employee.java
@@ -2,4 +2,12 @@
 package StaticVariable;
 
 public class Employee {
+    int Reg_no;
+    String Name;
+    static String Company = "Gstevewall";
+
+    Employee(int a, String b) {
+        Reg_no = a;
+        Name = b;
+    }
 }
