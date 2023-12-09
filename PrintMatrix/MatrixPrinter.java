78640710e42ff58e1ed45e146492162229e5fcd1
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-08 21:26:38 -0600

    Added an int array "num1" that is two dimensional and has 3 rows and 3 columns (3x3 matrix).

--- PrintMatrix/MatrixPrinter.java
+++ PrintMatrix/MatrixPrinter.java
@@ -8,5 +8,8 @@ public class MatrixPrinter {
 
     public static void main(String args[]) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int num1[][] = new int[3][3];
+
     }
 }
