6a8031a83087b9c3f04043c2f031c7ab858f07fe
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-09 12:09:52 -0600

    Made psvMain with exceptions, then added new buffer reader.

--- MatrixSubtraction/SubtractMatrix.java
+++ MatrixSubtraction/SubtractMatrix.java
@@ -1,4 +1,12 @@
 package MatrixSubtraction;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class SubtractMatrix {
+    public static void main(String args[]) throws NumberFormatException, IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
 }
