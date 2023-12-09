59861b136d98c5a639513cca57d57485cfcc63b7
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-09 11:33:04 -0600

    Added psvMain and created a new Buffer Reader to read input.

--- MatrixAddition/AddMatrix.java
+++ MatrixAddition/AddMatrix.java
@@ -1,4 +1,11 @@
 package MatrixAddition;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class AddMatrix {
+    public static void main(String args[]) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
 }
