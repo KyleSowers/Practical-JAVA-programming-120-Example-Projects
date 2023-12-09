14b1f96d4c00beb77eed03610d9eed7d3eeeef5b
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-08 21:42:37 -0600

    Added for loop to read user input to populate the matrix and to print the matrix.

--- PrintMatrix/MatrixPrinter.java
+++ PrintMatrix/MatrixPrinter.java
@@ -12,6 +12,20 @@ public class MatrixPrinter {
         int num1[][] = new int[3][3];
         int x, y;
         System.out.println("Please, enter 9 Integer values: ");
-
+        for (x = 0; x < 3; x++)
+        {
+            for(y = 0; y < 3; y++)
+            {
+                num1[x][y] = Integer.parseInt(br.readLine());
+            }
+        }
+    for (x = 0; x < num1.length; x++)
+    {
+        for (y = 0; y < 3; y++)
+        {
+            System.out.print(" " + num1[x][y]);
+        }
+        System.out.println();
     }
+}
 }
