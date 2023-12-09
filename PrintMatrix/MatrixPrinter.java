54b13ed57075bc58c57ca281155457261a4a91a2
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-08 21:45:04 -0600

    Added comments to explain code.

--- PrintMatrix/MatrixPrinter.java
+++ PrintMatrix/MatrixPrinter.java
@@ -1,3 +1,4 @@
+//PRACTICE 17: A PROGRAM TO PRINT NUMBERS IN MATRIX FORMAT
 package PrintMatrix;
 
 import java.io.BufferedReader;
@@ -8,10 +9,11 @@ public class MatrixPrinter {
 
     public static void main(String args[]) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        // Initializing a 3x3 matrix
         int num1[][] = new int[3][3];
         int x, y;
         System.out.println("Please, enter 9 Integer values: ");
+        // Reading user input to populate the matrix
         for (x = 0; x < 3; x++)
         {
             for(y = 0; y < 3; y++)
@@ -19,13 +21,14 @@ public class MatrixPrinter {
                 num1[x][y] = Integer.parseInt(br.readLine());
             }
         }
+        // Printing the matrix
     for (x = 0; x < num1.length; x++)
     {
         for (y = 0; y < 3; y++)
         {
             System.out.print(" " + num1[x][y]);
         }
-        System.out.println();
+        System.out.println(); // Move to the next line after each row
     }
 }
 }
