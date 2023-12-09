a5db4a7103734147cde5f9126783967db88824d2
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-08 21:12:20 -0600

    Added a void method "show()" that outputs results for values to console.

--- ThisKeyword/UseThis.java
+++ ThisKeyword/UseThis.java
@@ -8,4 +8,9 @@ public class UseThis {
         this.num1 = num1;
         this.num2 = num2;
     }
+
+    void show() {
+        System.out.println("Value of Num1 is: " + num1);
+        System.out.println("value of Num2 is: " + num2);
+    }
 }
