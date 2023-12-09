9fa765f896643ed3ea867e436507a8b4079b1fc1
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-09 12:13:11 -0600

    Added num 1, 2, 3 that are new matrices, or nested arrays 3x3. Then declared instance variables x and y, which will serve the nested loops needed for matrix completion.

--- MatrixSubtraction/SubtractMatrix.java
+++ MatrixSubtraction/SubtractMatrix.java
@@ -8,5 +8,10 @@ public class SubtractMatrix {
     public static void main(String args[]) throws NumberFormatException, IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num1[][] = new int[3][3];
+        int num2[][] = new int[3][3];
+        int num3[][] = new int[3][3];
+
+        int x, y;
     }
 }
