6a8fec27a8cc56e4353af38d139347b044152b6b
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-08 20:45:04 -0600

    Created instance variables x and y, with a constructor "FirstSuper" that takes two int values and stores into the x and y variables.

--- SuperClass/UserSuper.java
+++ SuperClass/UserSuper.java
@@ -1,6 +1,10 @@
 //PRACTICE 15: A PROGRAM FOR SUPER CLASS
 package SuperClass;
 
-public class FirstSuper {
-
+class FirstSuper {
+    int x, y;
+    FirstSuper(int a, int b) {
+        x = a;
+        y = b;
+    }
 }
