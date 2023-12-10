2608d5e9b91485cba898a15d5d35f893ec0fdc48
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-10 15:58:43 -0600

    Added 3 new int nested arrays that are 4x4. Then declared instance variables x, y, z for use in looping for multiplication of matrices.

--- MatrixMultiplication/MultiplyMatrix.java
+++ MatrixMultiplication/MultiplyMatrix.java
@@ -8,5 +8,11 @@ import java.nio.Buffer;
 public class MultiplyMatrix {
     public static void main(String args[]) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int num1[][] = new int[4][4];
+        int num2[][] = new int[4][4];
+        int num3[][] = new int[4][4];
+
+        int x, y, z;
     }
 }
