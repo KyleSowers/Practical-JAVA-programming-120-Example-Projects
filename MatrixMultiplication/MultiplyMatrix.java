3a4f42cd2676b38adb5ba90df07aec3e8287532e
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-10 15:55:33 -0600

    Added the psvMain with throws, then added new buffer reader for inputs.

--- MatrixMultiplication/MultiplyMatrix.java
+++ MatrixMultiplication/MultiplyMatrix.java
@@ -1,4 +1,12 @@
 package MatrixMultiplication;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+
 public class MultiplyMatrix {
+    public static void main(String args[]) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
 }
