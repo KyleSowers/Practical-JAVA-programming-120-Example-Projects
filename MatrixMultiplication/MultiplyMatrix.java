352925f9c14dc55e6501f97ace8a9e98f1e0d15f
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-10 17:39:35 -0600

    Created program for first matrix. Output request for values from user. then established a nested loop to read input and create the matrix with input values. Then added a second nested loop for reading the created num1[][] matrix and printing matrix result.

--- MatrixMultiplication/MultiplyMatrix.java
+++ MatrixMultiplication/MultiplyMatrix.java
@@ -14,5 +14,20 @@ public class MultiplyMatrix {
         int num3[][] = new int[4][4];
 
         int x, y, z;
+
+        System.out.println("Please, enter First Matrix");
+        for (x = 0; x < 4; x++) {
+            for (y = 0; y < 4; y++) {
+                num1[x][y] = Integer.parseInt(br.readLine());
+            }
+        }
+        for  ( x = 0; x < num1.length; x++) {
+            for  (y = 0; y < 4; y++) {
+                System.out.print(" " + num1[x][y]);
+            }
+            System.out.println();
+        }
+
+
     }
 }
