50413c94c5899f87b6bd3a6a65065f97abc175c7
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-10 18:14:19 -0600

    Added nested for loop that starts x at value of 7, then a condition of x >=1, with a decrementor. Nested with for y valued 7, where y > x that will decrement.' '

--- Triangle/Pattern.java
+++ Triangle/Pattern.java
@@ -3,4 +3,10 @@ package Triangle;
 public class Pattern {
 
     int x, y, z;
+    for (x = 7; x >= 1; x--){
+        for (y = 7; y > x; y--) {
+            System.out.print(" ");
+        }
+
+    }
 }
