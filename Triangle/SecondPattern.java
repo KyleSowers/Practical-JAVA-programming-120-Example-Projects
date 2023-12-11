2a73db8029b26ecdaf58b0b30b4b9f6ed8cd78cc
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-10 18:34:18 -0600

    added nested loop where outer loop sets x to 7, using the condition the x is greater than or equal to 1, it with post decrement. The inner loop is y set at 1, while y < x, will post increment to output the missing spaces.

--- Triangle/SecondPattern.java
+++ Triangle/SecondPattern.java
@@ -4,5 +4,10 @@ public class SecondPattern {
     public static void main(String args[]) {
 
         int x, y, z;
+        for (x = 7; x >= 1; x++){
+            for (y = 1; y < x; y++) {
+
+            }
+        }
     }
 }
