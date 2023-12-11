b5d50773c85b4188c7b283e5eb656fdcc44f98ca
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-10 18:18:19 -0600

    Added the missing psvMain and corrected the second print statement from creating extra lines (println --> print).

--- Triangle/Pattern.java
+++ Triangle/Pattern.java
@@ -1,15 +1,16 @@
 package Triangle;
 
 public class Pattern {
-
-    int x, y, z;
-    for (x = 7; x >= 1; x--){
-        for (y = 7; y > x; y--) {
-            System.out.print(" ");
+    public static void main(String args[]) {
+        int x, y, z;
+        for (x = 7; x >= 1; x--) {
+            for (y = 7; y > x; y--) {
+                System.out.print(" ");
+            }
+            for (z = 1; z <= x; z++) {
+                System.out.print("*");
+            }
+            System.out.println();
         }
-        for (z = 1; z <= x; z++) {
-            System.out.println("*");
-        }
-        System.out.println();
     }
 }
