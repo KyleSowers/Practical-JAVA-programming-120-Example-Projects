7fcc56e8d91c2d774bfe504d1205fa2d275d854d
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-11 20:45:00 -0600

    Added the print statement that provides astericts by x value and print statement that devides the loop outputs by line.

--- Triangle/ThirdPattern.java
+++ Triangle/ThirdPattern.java
@@ -6,8 +6,9 @@ public class ThirdPattern {
         int x, y;
         for (x = 7; x >= 1; x--) {
             for (y = 1; y <= x; y++) {
-
+                System.out.print("*");
             }
+            System.out.println();
         }
     }
 }
