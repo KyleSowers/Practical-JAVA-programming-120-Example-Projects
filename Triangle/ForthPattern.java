6787472b2f775d711e2e8d7ed9eb93d7e040c1a5
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-11 20:57:43 -0600

    Added line comments for code.

--- Triangle/ForthPattern.java
+++ Triangle/ForthPattern.java
@@ -2,12 +2,15 @@ package Triangle;
 
 public class ForthPattern {
     public static void main(String args[]) {
-
+            //declare instance variables
             int x, y;
+            //loop for value of asterisks, starting at value 7, post decremented, not lower than 1
             for (x = 7; x <= 7; x++) {
                 for (y = 1; y <= x; y++) {
+                    //print statement for value of asterisks determined by outer x for loop
                     System.out.print("*");
                 }
+                //Prints separating each iteration by new line to make the triangle
                 System.out.println();
             }
     }
