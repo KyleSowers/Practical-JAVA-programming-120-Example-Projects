033b64af103dee1660b9b1a07b1539d636bb23bf
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-11 20:54:36 -0600

    The number of asterisks at each iteration of the outerloop will print through this print statement.

--- Triangle/ForthPattern.java
+++ Triangle/ForthPattern.java
@@ -4,5 +4,10 @@ public class ForthPattern {
     public static void main(String args[]) {
 
             int x, y;
+            for (x = 7; x <= 7; x++) {
+                for (y = 1; y <= x; y++) {
+                    System.out.print("*");
+                }
+            }
     }
 }
