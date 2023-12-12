6bccf31b540654bc91283a3d41c08136caa53335
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-11 20:43:14 -0600

    Created nested for loop where the of x will determine output of astericts and y will determine empty strings, but will not be entirely neccesary for desired triangle foration, other than breaking the lines.

--- Triangle/ThirdPattern.java
+++ Triangle/ThirdPattern.java
@@ -2,6 +2,12 @@ package Triangle;
 
 public class ThirdPattern {
     public static void main(String args[]) {
+
         int x, y;
+        for (x = 7; x >= 1; x--) {
+            for (y = 1; y <= x; y++) {
+
+            }
+        }
     }
 }
