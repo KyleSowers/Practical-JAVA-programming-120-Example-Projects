68248a4a566b77c965103e94eae7f405ce356da9
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-11 20:46:14 -0600

    Added line coments

--- Triangle/ThirdPattern.java
+++ Triangle/ThirdPattern.java
@@ -2,8 +2,9 @@ package Triangle;
 
 public class ThirdPattern {
     public static void main(String args[]) {
-
+        //declare instance variables
         int x, y;
+        //nested loop to determine output values with print statements
         for (x = 7; x >= 1; x--) {
             for (y = 1; y <= x; y++) {
                 System.out.print("*");
