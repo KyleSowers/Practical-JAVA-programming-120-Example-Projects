0ea5f4e199b351929c40c446fdab45f9cd15b33f
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-12 21:10:41 -0600

    Added the looping structure to output the pascal triangle.

--- Triangle/Paschal.java
+++ Triangle/Paschal.java
@@ -12,5 +12,14 @@ public class Paschal {
 
         x = sc.nextInt();
         System.out.println("result of Paschal Triangle is: ");
+        for (y = 0; y < x; y++) {
+            int num1 = 2;
+            System.out.format("%" + (x - y) * 2 + "s", "");
+            for  (z = 0; z <= y; z++) {
+                System.out.format("%4d", num1);
+                num1 = num1 * (y - z) / (z + 1);
+            }
+            System.out.println();
+        }
     }
 }
