0299e2be41671e2dae6121c192522f3198fe730e
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-12 21:02:53 -0600

    Added new scanner for system input.

--- Triangle/Paschal.java
+++ Triangle/Paschal.java
@@ -1,4 +1,12 @@
 package Triangle;
 
+import java.util.Scanner;
+
 public class Paschal {
+
+    public static void main(String args[]) {
+
+        int x, y, z;
+        Scanner sc = new Scanner(System.in);
+    }
 }
