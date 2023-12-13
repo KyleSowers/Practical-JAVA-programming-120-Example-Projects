c7c50511fac064f98614bae5929ec4a58d7dae5d
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-12 21:23:05 -0600

    Added new scanner to read system input.

--- Triangle/Floyds.java
+++ Triangle/Floyds.java
@@ -1,9 +1,12 @@
 package Triangle;
 
+import java.util.Scanner;
+
 public class Floyds {
 
     public static void main(String args[]) {
 
         int x, num1 = 1, y, z;
+        Scanner sc = new Scanner(System.in);
     }
 }
