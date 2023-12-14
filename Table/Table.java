6f61fa8e8f2b04cde3ebbab25a7b006dcfa57017
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-13 21:15:34 -0600

    Added new scanner to read/accept user input.

--- Table/Table.java
+++ Table/Table.java
@@ -1,8 +1,12 @@
 package Table;
 
+import java.util.Scanner;
+
 public class Table {
     public static void main(String args[]) {
         int x, y;
         System.out.println("Please insert a number to print Multiplication Table");
+
+        Scanner sc = new Scanner(System.in);
     }
 }
