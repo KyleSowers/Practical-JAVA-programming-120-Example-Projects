70934d1ba3237c6e423eea3479c23002ac59908d
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-13 21:20:59 -0600

    Added line comments.

--- Table/Table.java
+++ Table/Table.java
@@ -4,12 +4,16 @@ import java.util.Scanner;
 
 public class Table {
     public static void main(String args[]) {
+        //declare instance variables
         int x, y;
+        //request number to use on multiplication table
         System.out.println("Please insert a number to print Multiplication Table");
-
+        //add new scanner
         Scanner sc = new Scanner(System.in);
+        //store input value
         x = sc.nextInt();
         System.out.println("Result of Multiplication Table of " + x + " is: ");
+        //loop for multiplying 1 - 20 and print statements
         for (y = 1; y <= 20; y++) {
             System.out.println(x + "*" + y + "=" + (x*y));
         }
