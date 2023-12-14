2fbe221d3db5f21f1e33b821a828ab54302cbe3f
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-13 21:14:50 -0600

    printed a statment to console to request a number for table.

--- Table/Table.java
+++ Table/Table.java
@@ -3,5 +3,6 @@ package Table;
 public class Table {
     public static void main(String args[]) {
         int x, y;
+        System.out.println("Please insert a number to print Multiplication Table");
     }
 }
