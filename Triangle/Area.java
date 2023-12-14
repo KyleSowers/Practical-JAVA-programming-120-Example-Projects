653ea91b7a3fcefeddba394952cfd3b143a2ed57
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-13 21:27:55 -0600

    Added a print request for values width and height.

--- Triangle/Area.java
+++ Triangle/Area.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Area {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("PLease enter a Width and Height of the Triangle: ");
         double w = sc.nextDouble();
         double h = sc.nextDouble();
 
