5d12645383e35757814ff2c730036a66f617c1ee
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-13 21:19:14 -0600

    Added print statement and loop with print statement to ouput table of value input x, to each number up to twenty multipied in a table.

--- Table/Table.java
+++ Table/Table.java
@@ -9,5 +9,9 @@ public class Table {
 
         Scanner sc = new Scanner(System.in);
         x = sc.nextInt();
+        System.out.println("Result of Multiplication Table of " + x + " is: ");
+        for (y = 1; y <= 20; y++) {
+            System.out.println(x + "*" + y + "=" + (x*y));
+        }
     }
 }
