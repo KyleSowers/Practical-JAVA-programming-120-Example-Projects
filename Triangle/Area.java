fb1fcaef42034c834415ddedd7cff2d7d7de87b9
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-13 21:29:33 -0600

    Added line comments.

--- Triangle/Area.java
+++ Triangle/Area.java
@@ -4,12 +4,16 @@ import java.util.Scanner;
 
 public class Area {
     public static void main(String args[]) {
+        //create new scanner
         Scanner sc = new Scanner(System.in);
+        //request width and height values
         System.out.println("PLease enter a Width and Height of the Triangle: ");
+        //store width and height values
         double w = sc.nextDouble();
         double h = sc.nextDouble();
-
+        //store equation value for area of triangle
         double result = (w*h)/2;
+        //display results
         System.out.println("Result of area of triangle is: " + result);
     }
 }
