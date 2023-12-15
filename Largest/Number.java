9d78ff29980b2672c980853cef67ee4809686456
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-14 20:59:31 -0600

    Printed request to user for three interger numbers.

--- Largest/Number.java
+++ Largest/Number.java
@@ -3,5 +3,6 @@ package Largest;
 public class Number {
     public static void main(String args[]) {
         int x, y, z;
+        System.out.println("PLease insert 3 different integer numbers: ");
     }
 }
