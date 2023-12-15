4c1fbf5052f6db636b87032e6d5ac419169833e8
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-14 21:13:40 -0600

    Created/added new buffer reader.

--- Maths/Operators.java
+++ Maths/Operators.java
@@ -1,10 +1,14 @@
 //PRACTICE 30: A PROGRAM FOR USE OF MATH OPERATORS FOR CALCULATOR
 package Maths;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Operators {
-    public static void main(String args[]) throw NumberFormatException, IOException {
+    public static void main(String args[]) throws NumberFormatException, IOException {
+
         int x, y, z, xyz = 0;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     }
 }
