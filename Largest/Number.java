56e3d16df4fb8128643ea647b588e692e475aa58
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-14 21:00:20 -0600

    Created new scanner to read/accept input from user.

--- Largest/Number.java
+++ Largest/Number.java
@@ -1,8 +1,11 @@
 package Largest;
 
+import java.util.Scanner;
+
 public class Number {
     public static void main(String args[]) {
         int x, y, z;
         System.out.println("PLease insert 3 different integer numbers: ");
+        Scanner sc = new Scanner(System.in);
     }
 }
