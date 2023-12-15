681c67c77cda0fc388077097b83767d7d6b48ff4
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-14 21:12:38 -0600

    Added psvMain and declared instance variables x, y, z, and set variable xyz with a value of 0.

--- Maths/Operators.java
+++ Maths/Operators.java
@@ -1,5 +1,10 @@
 //PRACTICE 30: A PROGRAM FOR USE OF MATH OPERATORS FOR CALCULATOR
 package Maths;
 
+import java.io.IOException;
+
 public class Operators {
+    public static void main(String args[]) throw NumberFormatException, IOException {
+        int x, y, z, xyz = 0;
+    }
 }
