3fa5e075f9bb9511bb36a62e54328dc4da85ce4b
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-14 21:01:29 -0600

    Storing inputs into the instance variables as their values.

--- Largest/Number.java
+++ Largest/Number.java
@@ -7,5 +7,8 @@ public class Number {
         int x, y, z;
         System.out.println("PLease insert 3 different integer numbers: ");
         Scanner sc = new Scanner(System.in);
+        x = sc.nextInt();
+        y = sc.nextInt();
+        z = sc.nextInt();
     }
 }
