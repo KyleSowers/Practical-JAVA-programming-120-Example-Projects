94033a617dc700db0dbb71c7d1e6a3b66f17c2b2
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-14 21:05:15 -0600

    Added if-else if-else condition that will determine which number is the highest and output the result, including if the same numbers are used.

--- Largest/Number.java
+++ Largest/Number.java
@@ -10,5 +10,15 @@ public class Number {
         x = sc.nextInt();
         y = sc.nextInt();
         z = sc.nextInt();
+
+        if (x > y && x > z) {
+            System.out.println("Number one is the largest");
+        } else if (y > x && y > z) {
+            System.out.println("Number two is the largest");
+        } else if (z > x && z > y) {
+            System.out.println("Number three is the largest");
+        } else {
+            System.out.println("These numbers are the same");
+        }
     }
 }
