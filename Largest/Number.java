219d924bc677ed7a40df32f75e7edb0fa1ea246f
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-14 21:07:26 -0600

    Added line comments to explain the code.

--- Largest/Number.java
+++ Largest/Number.java
@@ -1,16 +1,21 @@
+//PRACTICE 29:  A PROGRAM TO DETECT LARGEST NUMBER INPUT FROM A USER
 package Largest;
 
 import java.util.Scanner;
 
 public class Number {
     public static void main(String args[]) {
+        //declare the instance variables to be used
         int x, y, z;
+        //request user to provide values
         System.out.println("PLease insert 3 different integer numbers: ");
+        //add new scanner to read user input
         Scanner sc = new Scanner(System.in);
+        //store user values
         x = sc.nextInt();
         y = sc.nextInt();
         z = sc.nextInt();
-
+        //condition if-else if-else to determine the largest and give correct print statement
         if (x > y && x > z) {
             System.out.println("Number one is the largest");
         } else if (y > x && y > z) {
