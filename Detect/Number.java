9bc4b3bad12a31960fa90a38f6b14bec1910390e
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-16 10:35:10 -0600

    Added line comments.

--- Detect/Number.java
+++ Detect/Number.java
@@ -4,11 +4,15 @@ import java.util.Scanner;
 
 public class Number {
     public static void main(String args[]) {
+        //declare instance variable
         int x;
+        //request input from user
         System.out.println("Please insert any number: ");
+        //create new scanner
         Scanner sc = new Scanner(System.in);
+        //store input value into variable
         x =sc.nextInt();
-
+        //check if negative, else positive, print appropriate result
         if (x < 0) {
             System.out.println("This is a negative number.");
         } else {
