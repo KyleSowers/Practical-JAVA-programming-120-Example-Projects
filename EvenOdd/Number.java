8783685f278bb699c8011807fc5b69201812c2b9
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-16 10:41:35 -0600

    Created new scanner to accept input from user.

--- EvenOdd/Number.java
+++ EvenOdd/Number.java
@@ -1,9 +1,11 @@
 package EvenOdd;
 
+import java.util.Scanner;
+
 public class Number {
     public static void main(String args[]) {
         int x;
         System.out.println("Please provide a number");
-
+        Scanner sc = new Scanner(System.in);
     }
 }
