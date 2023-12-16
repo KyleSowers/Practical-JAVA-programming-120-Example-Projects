ad0d51a338a235a27a0a398c01330f94651fc649
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-15 21:55:15 -0600

    Added new scanner to read user input.

--- Factorial/Number.java
+++ Factorial/Number.java
@@ -1,8 +1,12 @@
 package Factorial;
 
+import java.util.Scanner;
+
 public class Number {
     public static void main(String args[]) {
         int x, num1, num2 = 1;
         System.out.println("Please insert a number");
+        Scanner sc = new Scanner(System.in);
+
     }
 }
