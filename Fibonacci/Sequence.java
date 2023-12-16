dd8f451674803e06a29579f1bf7dcea0e9aaf6d8
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-16 11:00:38 -0600

    Added print staement for string output of result.

--- Fibonacci/Sequence.java
+++ Fibonacci/Sequence.java
@@ -3,5 +3,6 @@ package Fibonacci;
 public class Sequence {
     public static void main(String args[]) {
         int w = 0, x = 1, y, z, count =10;
+        System.out.println("The result of the Fibonacci Sequence is: ");
     }
 }
