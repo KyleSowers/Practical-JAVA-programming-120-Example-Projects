78efd9834daff7df8e73f673636d98e3dc25826b
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-16 10:30:00 -0600

    Added new scanner to read and accept user input.

--- Detect/Number.java
+++ Detect/Number.java
@@ -1,8 +1,11 @@
 package Detect;
 
+import java.util.Scanner;
+
 public class Number {
     public static void main(String args[]) {
         int x;
         System.out.println("Please insert any number: ");
+        Scanner sc = new Scanner(System.in);
     }
 }
