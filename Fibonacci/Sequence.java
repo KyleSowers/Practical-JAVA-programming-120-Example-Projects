35538f53386820d6dcee33ce9b2bd61db989ed39
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-16 11:02:08 -0600

    Made print statemnt for int results.

--- Fibonacci/Sequence.java
+++ Fibonacci/Sequence.java
@@ -4,5 +4,6 @@ public class Sequence {
     public static void main(String args[]) {
         int w = 0, x = 1, y, z, count =10;
         System.out.println("The result of the Fibonacci Sequence is: ");
+        System.out.print(w + " " + x);
     }
 }
