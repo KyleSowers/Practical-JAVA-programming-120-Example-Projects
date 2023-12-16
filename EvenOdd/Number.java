8d1f1295e9ae3ad13b35a9c67bae8eb93d0d116b
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-16 10:48:38 -0600

    Added line comments explaining code.

--- EvenOdd/Number.java
+++ EvenOdd/Number.java
@@ -4,11 +4,15 @@ import java.util.Scanner;
 
 public class Number {
     public static void main(String args[]) {
+        //declare instance variable
         int x;
+        //request number from user
         System.out.println("Please provide a number");
+        //create scanner to read input
         Scanner sc = new Scanner(System.in);
+        //store user input value into variable
         x = sc.nextInt();
-
+        //if-else to check with modulus operator if x is even or odd and print correct output
         if (x % 2 == 0) {
             System.out.println(x + " is even.");
         } else {
