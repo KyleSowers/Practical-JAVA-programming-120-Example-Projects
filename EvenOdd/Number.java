094804ac0c52735faaa77555d98400c0901921d3
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-16 10:46:30 -0600

    Added if-else statement to first check if x is even using the modulus operator by value of 2 to print out even statement, else print out odd statement.

--- EvenOdd/Number.java
+++ EvenOdd/Number.java
@@ -8,5 +8,11 @@ public class Number {
         System.out.println("Please provide a number");
         Scanner sc = new Scanner(System.in);
         x = sc.nextInt();
+
+        if (x % 2 == 0) {
+            System.out.println(x + " is even.");
+        } else {
+            System.out.println(x + " is odd.");
+        }
     }
 }
