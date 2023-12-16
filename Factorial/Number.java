d33189ca6fb98d9efa27912ee36eca6126d93336
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-15 22:04:43 -0600

    Used an if-else statment to first verify number is positive, or a print statment would display addressing need for positive number. The a nested while loop in the else ( positive number) that checks if greater or equal to 1, than 1 * input is stored into num2 as new value, then the input value is reduced by 1 and rerun through the while statment to check until value is lower than one to exit. When exits, a print statemnt produces statement with initial input and its factorial value.

--- Factorial/Number.java
+++ Factorial/Number.java
@@ -9,5 +9,14 @@ public class Number {
         Scanner sc = new Scanner(System.in);
         num1 = sc.nextInt();
         x = num1;
+        if (num1 < 0) {
+            System.out.println("PLease insert only positive numbers");
+        } else {
+            while (num1 >= 1) {
+                num2 *= num1;
+                num1 -= 1;
+            }
+            System.out.println("Result of Factorial of " + x + " is: " + num2);
+        }
     }
 }
