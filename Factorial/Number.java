e58d7ac4d13386c8ea9176e15c3549396f22cdc5
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-15 21:54:29 -0600

    Made a print statement requesting the input of a number.

--- Factorial/Number.java
+++ Factorial/Number.java
@@ -3,5 +3,6 @@ package Factorial;
 public class Number {
     public static void main(String args[]) {
         int x, num1, num2 = 1;
+        System.out.println("Please insert a number");
     }
 }
