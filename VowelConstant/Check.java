acfba26e9310e7369fc38d9e80e0581898ccd219
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-17 22:47:42 -0500

    Added a switch case that identifies any lower or upper case vowel, but has a default to address otherwise consonant character.

--- VowelConstant/Check.java
+++ VowelConstant/Check.java
@@ -11,7 +11,41 @@ public class Check {
         try {
             c = (char)br.read();
         } catch (Exception e) {
-
+            switch(c) {
+                case 'a':
+                    System.out.println(c + " is a vowel");
+                    break;
+                case 'e':
+                    System.out.println(c + " is a vowel");
+                    break;
+                case 'i':
+                    System.out.println(c + " is a vowel");
+                    break;
+                case 'o':
+                    System.out.println(c + " is a vowel");
+                    break;
+                case 'u':
+                    System.out.println(c + " is a vowel");
+                    break;
+                case 'A':
+                    System.out.println(c + " is a vowel");
+                    break;
+                case 'E':
+                    System.out.println(c + " is a vowel");
+                    break;
+                case 'I':
+                    System.out.println(c + " is a vowel");
+                    break;
+                case "O":
+                    System.out.println(c + " is a vowel");
+                    break;
+                case "U":
+                    System.out.println(c + " is a vowel");
+                    break;
+                default:
+                    System.out.println(c + " is a consonant");
+                    break;
+            }
         }
     }
 }
