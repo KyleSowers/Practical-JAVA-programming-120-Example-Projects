803b41f6ae65ba281386b46fb6e68f6e72e5e9b9
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-17 23:20:52 -0500

    Corrected array brackets and added line one comment of practice titile.

--- PrintArray/Array.java
+++ PrintArray/Array.java
@@ -1,9 +1,10 @@
+//PRACTICE 38: A PROGRAM TO PRINT OUT ARRAYS
 package PrintArray;
 
 public class Array {
     public static void main(String args[]) {
         //declare array with values
-        int x[] = (1, 2, 3, 4, 5, 6, 7, 8);
+        int x[] = {1, 2, 3, 4, 5, 6, 7, 8};
         //initial print statement of a string
         System.out.println("Our Array is: ");
         //for loop to iterate through array length and print each index
