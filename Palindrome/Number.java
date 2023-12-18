5559127ac570d19755fcf15ca1bb5c026b2dfad3
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-17 21:57:21 -0500

    Made print statement to request numbers to check Pallindrome.

--- Palindrome/Number.java
+++ Palindrome/Number.java
@@ -6,5 +6,6 @@ public class Number {
     public static void main(String args[]) {
         int w, x, y = 0, z = 0;
         Scanner sc = new Scanner(System.in);
+        System.out.println("Please insert numbers to check Palindrome: ");
     }
 }
