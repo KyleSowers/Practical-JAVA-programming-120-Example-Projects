738e3de97e01d98f0ec6757390f21e1a170c6414
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-17 22:30:41 -0500

    Created a new buffer reader for class.

--- VowelConstant/Check.java
+++ VowelConstant/Check.java
@@ -1,7 +1,11 @@
 package VowelConstant;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Check {
     public static void main(String args[]) {
         char c = ' ';
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     }
 }
