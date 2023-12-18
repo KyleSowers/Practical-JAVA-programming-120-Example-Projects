39ba7cc33a7eb353caa822919eb5aa5873af260f
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-17 22:13:48 -0500

    Added a while loop that takes the last number in z, adds it as the next number in y, which wil create a reverse copy of original input, which will be used to identify pallidrome.

--- Palindrome/Number.java
+++ Palindrome/Number.java
@@ -9,5 +9,11 @@ public class Number {
         System.out.println("Please insert numbers to check Palindrome: ");
         z = sc.nextInt();
         w = z;
+
+        while (z > 0) {
+            x = z % 10;
+            y = (y * 10) + x;
+            z = z / 10;
+        }
     }
 }
