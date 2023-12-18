9eb2b055b75a0957d195806bea5d425383709f91
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-17 23:05:54 -0500

    Print statement that presumes  statement for output of array.

--- PrintArray/Array.java
+++ PrintArray/Array.java
@@ -3,5 +3,6 @@ package PrintArray;
 public class Array {
     public static void main(String args[]) {
         int x[] = (1, 2, 3, 4, 5, 6, 7, 8);
+        System.out.println("Our Array is: ");
     }
 }
