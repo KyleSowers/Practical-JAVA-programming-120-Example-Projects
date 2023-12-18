3de831d389b37d97451e19bef4f8bf63e5a4cb02
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-17 23:11:32 -0500

    Added line comments.

--- PrintArray/Array.java
+++ PrintArray/Array.java
@@ -2,8 +2,11 @@ package PrintArray;
 
 public class Array {
     public static void main(String args[]) {
+        //declare array with values
         int x[] = (1, 2, 3, 4, 5, 6, 7, 8);
+        //initial print statement of a string
         System.out.println("Our Array is: ");
+        //for loop to iterate through array length and print each index
         for (int y = 0; y < x.length; y++) {
             System.out.println(" " + x[y]);
         }
