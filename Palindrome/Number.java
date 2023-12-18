697a0c6a2d85f95ebae39c143be1d6ac07650abd
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-17 21:55:43 -0500

    Created a new scanner for the class.

--- Palindrome/Number.java
+++ Palindrome/Number.java
@@ -1,7 +1,10 @@
 package Palindrome;
 
+import java.util.Scanner;
+
 public class Number {
     public static void main(String args[]) {
         int w, x, y = 0, z = 0;
+        Scanner sc = new Scanner(System.in);
     }
 }
