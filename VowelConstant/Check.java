fd1c3a2ed25cfeb3847a94bbfd083f62124aed4f
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-17 22:56:11 -0500

    Added line comments for code.

--- VowelConstant/Check.java
+++ VowelConstant/Check.java
@@ -5,14 +5,20 @@ import java.io.InputStreamReader;
 
 public class Check {
     public static void main(String args[]) {
+        //declare char instance variable
         char c = ' ';
+        //create new buffer reader
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //request character from user
         System.out.print("Please insert a character: ");
+        //check if input is char
         try {
             c = (char)br.read();
         } catch (Exception e) {
+            //throw exception if not char
             System.out.println(e);
         }
+        //use switch cases to identify char vowels and default for consonants
             switch(c) {
                 case 'a':
                     System.out.println(c + " is a vowel");
