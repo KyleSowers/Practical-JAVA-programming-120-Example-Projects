78dbfe9741246a321efe9ffc85792a46c4c06dd9
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-18 23:07:47 -0500

    Added line comments for code

--- StringArray/Array.java
+++ StringArray/Array.java
@@ -6,13 +6,17 @@ import java.io.InputStreamReader;
 
 public class Array {
     public static void main(String args[]) throws IOException {
+        //declare new string array
         String st[] = new String[7];
+        //create new buffer reader
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        //request array values from user
         System.out.println("Please, insert 7 string elements: ");
+        //for loop will use input to assign as array values
         for (int x = 0; x < 7; x++) {
             st[x] = br.readLine();
         }
+        //return result by for loop that reads array and outputs values with print statement
         System.out.print("The result of the string values are: ");
         {
             for (int x = 0; x < 7; x++) {
