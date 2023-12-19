b035b1d05ec1d62ae122d4c03a2b59c476d68567
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-18 22:26:51 -0500

    Created new bufferreader to read input.

--- AcceptArray/Array.java
+++ AcceptArray/Array.java
@@ -1,9 +1,14 @@
 package AcceptArray;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Array {
     public static void main(String args[]) throws NumberFormatException, IOException {
         int x[] = new int[7];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
     }
 }
