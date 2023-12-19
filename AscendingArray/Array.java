e3c1d219e6b009610bd04caa50b9b8e25e2b2fcd
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-18 22:40:24 -0500

    Made psvMain and declared insance variables and new array that will hold 7 integers.

--- AscendingArray/Array.java
+++ AscendingArray/Array.java
@@ -1,4 +1,9 @@
 package AscendingArray;
 
+import java.io.IOException;
+
 public class Array {
+    public static void main(String args[]) throws NumberFormatException, IOException {
+        int num1 = 0, num2[] = new int[7], x, y;
+    }
 }
