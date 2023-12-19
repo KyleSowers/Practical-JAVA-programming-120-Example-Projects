118dddb3b3e81e5f07df7d0f0b342db7ec336064
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-18 22:48:48 -0500

    Added another for loop that reads the elements, compares each tested element to the remaining and will sort if needed to create the ascending order within the array.

--- AscendingArray/Array.java
+++ AscendingArray/Array.java
@@ -13,5 +13,14 @@ public class Array {
         for ( x = 0; x < 7; x++) {
             num2[x] = Integer.parseInt(br.readLine());
         }
+        for (x = 0; x < num2.length; x++) {
+            for (y = x + 1; y < num2.length; y++) {
+                if (num2[x] > num2[y]) {
+                    num1 = num2[x];
+                    num2[x] = num2[y];
+                    num2[y] = num1;
+                }
+            }
+        }
     }
 }
