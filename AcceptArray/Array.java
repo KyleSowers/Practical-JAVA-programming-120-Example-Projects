4cb17a2e3d1d2c566cf601e2901f370714c4af0a
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-18 22:33:57 -0500

    Added line comments.

--- AcceptArray/Array.java
+++ AcceptArray/Array.java
@@ -6,13 +6,17 @@ import java.io.InputStreamReader;
 
 public class Array {
     public static void main(String args[]) throws NumberFormatException, IOException {
+        //declare instance array x, with a total integer (index) of 7 integers
         int x[] = new int[7];
+        //create new buffer reader
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        //request array values from user
         System.out.print("Please insert Array numbers: ");
+        //loop to create array from user values
         for (int y = 0; y < 7; y++) {
             x[y] = Integer.parseInt(br.readLine());
         }
+        //loop to read new array and output to user
         for  (int y = 0; y < x.length; y++) {
             System.out.print(" " + x[y]);
         }
