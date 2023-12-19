91f6ed10ff6961aada7e6096563d5e9676fdfb06
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-18 19:41:02 -0500

    Added psvMain and declared array x that can hold 7 integers.

--- AcceptArray/Array.java
+++ AcceptArray/Array.java
@@ -1,4 +1,9 @@
 package AcceptArray;
 
+import java.io.IOException;
+
 public class Array {
+    public static void main(String args[]) throws NumberFormatException, IOException {
+        int x[] = new int[7];
+    }
 }
