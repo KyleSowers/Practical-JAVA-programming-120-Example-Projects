5ab6d19af22297bdb8b6ea453d61d7764c4eebc6
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-18 22:42:20 -0500

    Added a print statement to request 7 array elements from user.

--- AscendingArray/Array.java
+++ AscendingArray/Array.java
@@ -8,5 +8,7 @@ public class Array {
     public static void main(String args[]) throws NumberFormatException, IOException {
         int num1 = 0, num2[] = new int[7], x, y;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("\n Please enter only seven array elements: ");
     }
 }
