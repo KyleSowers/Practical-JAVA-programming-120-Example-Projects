0b153b0021114e5fda812a282e8c6b6cac6af4b9
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-18 22:58:36 -0500

    Made class psvMain and declared new String array which can hold 7 string elements.

--- StringArray/Array.java
+++ StringArray/Array.java
@@ -1,4 +1,9 @@
 package StringArray;
 
+import java.io.IOException;
+
 public class Array {
+    public sealed void main(String args[]) throws IOException {
+        String st[] = new String[7];
+    }
 }
