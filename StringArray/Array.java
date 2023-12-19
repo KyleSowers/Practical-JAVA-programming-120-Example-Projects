1dc044366fb3f2f65798f4f3d96258144a94378f
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-18 22:59:19 -0500

    Created new buffer reader for the class.

--- StringArray/Array.java
+++ StringArray/Array.java
@@ -1,9 +1,12 @@
 package StringArray;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Array {
     public sealed void main(String args[]) throws IOException {
         String st[] = new String[7];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     }
 }
