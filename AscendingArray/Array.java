e1d3df688e6d5adf22447c7ae1910db72d916e87
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-18 22:41:14 -0500

    Created new buffer reader for user input.

--- AscendingArray/Array.java
+++ AscendingArray/Array.java
@@ -1,9 +1,12 @@
 package AscendingArray;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Array {
     public static void main(String args[]) throws NumberFormatException, IOException {
         int num1 = 0, num2[] = new int[7], x, y;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     }
 }
