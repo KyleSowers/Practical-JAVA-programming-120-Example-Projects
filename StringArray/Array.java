fe0e2507a406c66fb57b42b7900b4b6ce423ae97
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-18 23:05:42 -0500

    Added output for result, that involves a print staement follwed by a for loop that reads the array values and prints along same line as print statement.

--- StringArray/Array.java
+++ StringArray/Array.java
@@ -5,13 +5,19 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Array {
-    public sealed void main(String args[]) throws IOException {
+    public static void main(String args[]) throws IOException {
         String st[] = new String[7];
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         System.out.println("Please, insert 7 string elements: ");
         for (int x = 0; x < 7; x++) {
             st[x] = br.readLine();
+        }
+        System.out.print("The result of the string values are: ");
+        {
+            for (int x = 0; x < 7; x++) {
+                System.out.print(" " + st[x]);
+            }
         }
     }
 }
