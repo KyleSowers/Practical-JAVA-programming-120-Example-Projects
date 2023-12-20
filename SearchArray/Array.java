73e85eea9315648b73ed425beb6b0a3dda9db415
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-19 20:21:58 -0500

    Added print statement about the index found from the binary search.

--- SearchArray/Array.java
+++ SearchArray/Array.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class Array {
     public static void main(String args[]) {
         int x[] = {2, 6, 7, 9, 1, 8, 4};
-        int Index = Arrays.binarySearch(x, 1);
+        int index = Arrays.binarySearch(x, 1);
+        System.out.println("This element is found at index: " + index);
     }
 }
