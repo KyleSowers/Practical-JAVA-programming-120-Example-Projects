4f696eaaaec57d72d2948f794ba44c5b9a7dcf25
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-19 20:20:51 -0500

    used binary search method to identify the index of value 1 in x (the array).

--- SearchArray/Array.java
+++ SearchArray/Array.java
@@ -1,7 +1,10 @@
 package SearchArray;
 
+import java.util.Arrays;
+
 public class Array {
     public static void main(String args[]) {
         int x[] = {2, 6, 7, 9, 1, 8, 4};
+        int Index = Arrays.binarySearch(x, 1);
     }
 }
