41c9163596a68f77af7e16b9c35b447347eeb980
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-19 20:27:47 -0500

    Created new scanner for class.

--- InsertElement/Arrays.java
+++ InsertElement/Arrays.java
@@ -1,7 +1,10 @@
 package InsertElement;
 
+import java.util.Scanner;
+
 public class Arrays {
     public static void main(String args[]) {
         int x, y, z;
+        Scanner sc = new Scanner(System.in);
     }
 }
