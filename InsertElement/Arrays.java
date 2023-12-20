86d828b1b8ec64689dee58247e4a4d4429a89e30
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-19 20:28:46 -0500

    Added a print statement to request any element of choice from user.

--- InsertElement/Arrays.java
+++ InsertElement/Arrays.java
@@ -6,5 +6,8 @@ public class Arrays {
     public static void main(String args[]) {
         int x, y, z;
         Scanner sc = new Scanner(System.in);
+        System.out.println("Please insert any element of your choice: ");
+
+
     }
 }
