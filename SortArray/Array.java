0db140a1a63d8c7a1d2ce20a451b795b625be7aa
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-19 20:13:24 -0500

    Added line comments for code.

--- SortArray/Array.java
+++ SortArray/Array.java
@@ -2,17 +2,21 @@ package SortArray;
 
 import java.util.Arrays;
 
-import static com.sun.org.apache.bcel.internal.classfile.Utility.printArray;
-
 public class Array {
     public static void main(String args[]) {
+        //declare the new array with values
         int x[] = {3, 4, 8, 9, -4, 0, 5, 7, -9};
+        //sort array values
         Arrays.sort(x);
+        //print array with method from below
         printArray("The sorted Array is: ", x);
     }
 
+    //create the print method for the array
     private static void printArray(String string, int[] x) {
+        //loop to read through the array length
         for  (int y = 0; y < x.length; y++) {
+            //conditional for comma inserts
             if (y != 0) {
                 System.out.print(",");
             }
