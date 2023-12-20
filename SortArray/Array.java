7305a5ad749a7c3d5913422120a8adeb1172b203
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-19 20:04:32 -0500

    Called on the array sort method for the array.

--- SortArray/Array.java
+++ SortArray/Array.java
@@ -1,7 +1,10 @@
 package SortArray;
 
+import java.util.Arrays;
+
 public class Array {
     public static void main(String args[]) {
         int x[] = {3, 4, 8, 9, -4, 0, 5, 7, -9};
+        Arrays.sort(x);
     }
 }
