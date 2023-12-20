949e971352df99b6206bf0dfd037ce0c9411be8d
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-19 20:06:55 -0500

    Creeated print array statment and now creating print array method.

--- SortArray/Array.java
+++ SortArray/Array.java
@@ -2,9 +2,15 @@ package SortArray;
 
 import java.util.Arrays;
 
+import static com.sun.org.apache.bcel.internal.classfile.Utility.printArray;
+
 public class Array {
     public static void main(String args[]) {
         int x[] = {3, 4, 8, 9, -4, 0, 5, 7, -9};
         Arrays.sort(x);
+        printArray("The sorted Array is: ", x);
+    }
+
+    private static void printArray(String s, int[] x) {
     }
 }
