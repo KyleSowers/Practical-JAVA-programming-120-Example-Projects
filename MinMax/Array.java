5aaeb411c01c88d4f0a67756d14d4fcf6d8f788e
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-20 23:31:05 -0500

    Added line comments.

--- MinMax/Array.java
+++ MinMax/Array.java
@@ -6,10 +6,12 @@ import java.util.Collections;
 
 public class Array {
     public static void main(String[] args) {
+        //declared new integer array with values
         Integer[] num1 = {7,3,5,2,8};
-
+        //use inport collections library to use method to find min and max values and store in declared attributes
         int min = (int)Collections.min(Arrays.asList(num1));
         int max = (int)Collections.max(Arrays.asList(num1));
+        //print results
         System.out.println("Result of Minimum element is: " + min);
         System.out.println("Result of Maximum element is: " + max);
     }
