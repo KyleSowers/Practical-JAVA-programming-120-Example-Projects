acf947d8fa1d673d9b115cc13c23b8967d58dc79
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-21 14:36:24 -0500

    Declared new feild num1 that is an int and holds the value of upper - lower parameters.

--- MergeSort/Array.java
+++ MergeSort/Array.java
@@ -4,6 +4,9 @@ package MergeSort;
 public class Array {
 
     public static void sort(int[] x, int lower, int upper) {
-
+        int num1 = upper - lower;
+        if (num1 <= 1) {
+            return;
+        }
     }
 }
