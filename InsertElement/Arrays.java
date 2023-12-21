f3bd768610f8036bef361fa0e7612948d97e7305
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-20 23:00:13 -0500

    Added line comments for code roughly half way through practice.

--- InsertElement/Arrays.java
+++ InsertElement/Arrays.java
@@ -5,17 +5,25 @@ import java.util.Scanner;
 
 public class Arrays {
     public static void main(String args[]) {
+        //declare attributes(variables or fields)
         int x, y, z;
+        //create new scanner for class
         Scanner sc = new Scanner(System.in);
+        //print request to user for length of array
         System.out.println("Please insert any element amount of your choice: ");
-
+        //read and store user input value into attribute x
         x = sc.nextInt();
+        //declare new array
         int num1[] = new int[x + 1];
+        //print request element values from user
         System.out.println("Please insert array element values now: ");
+        //read, loop, and store input values into new array
         for (int w = 0; w < x; w++) {
             num1[w] = sc.nextInt();
         }
+        //print request for position of a new element from user
         System.out.println("Choose a preferred position for your new element: ");
+
         y = sc.nextInt();
         System.out.print("Insert your preferred element value: ");
         z = sc.nextInt();
