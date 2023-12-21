3edca1572fcb295e13b7eabe3b27870ce2b2b5fa
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-21 14:21:35 -0500

    Created new ArrayList that calls method to make the x array a list, then is called next line to add array y to the new list.

--- MergeArray/Array.java
+++ MergeArray/Array.java
@@ -1,9 +1,15 @@
 //PRACTICE 47: A PROGRAM TO MERGE TWO ARRAYS
 package MergeArray;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Array {
     public static void main(String[] args) {
         String x[] = {"H", "E", "L", "L", "O"};
         String y[] = {"W", "O", "R", "L", "D"};
+
+        List ls = new ArrayList(Arrays.aslist(x));
+        ls.addAll(Arrays.asList(y));
     }
 }
