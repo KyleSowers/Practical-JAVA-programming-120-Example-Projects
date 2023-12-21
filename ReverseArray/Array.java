212e69e851a5b46cca7f2e275cc4fd2c0a9fd457
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-20 23:18:10 -0500

    Added line comments.

--- ReverseArray/Array.java
+++ ReverseArray/Array.java
@@ -7,16 +7,19 @@ import java.util.Collections;
 
 public class Array {
     public static void main(String[] args) {
+        //create new arraylist
         ArrayList al = new ArrayList();
+        //add desired elements(values)
         al.add("E");
         al.add("J");
         al.add("I");
         al.add("K");
         al.add("E");
-
+        //print result of list before reversal
         System.out.println("Before reversal: " + al);
-
+        //call method for reversal
         Collections.reverse(al);
+        //print reversal
         System.out.println("After reversal: " + al);
     }
 }
