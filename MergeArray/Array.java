664fce38a28ca646c222b6de7b3c5711dff132df
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-21 14:29:50 -0500

    Added line comments.

--- MergeArray/Array.java
+++ MergeArray/Array.java
@@ -6,13 +6,16 @@ import java.util.*;
 
 public class Array {
     public static void main(String args[]) {
+        //declare two new String arrays
         String x[] = {"H", "E", "L", "L", "O"};
         String y[] = {"W", "O", "R", "L", "D"};
-
+        //create new List that takes in value of array x
         List ls = new ArrayList(Arrays.asList(x));
+        //combine in the value of array y to new list
         ls.addAll(Arrays.asList(y));
-
+        //create new array and convert new list to array
         Object[] z = ls.toArray();
+        //print array
         System.out.println(Arrays.toString(z));
     }
 }
