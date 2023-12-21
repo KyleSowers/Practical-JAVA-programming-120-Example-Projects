2d6ea9580f8e6e7ad8638c0872ed9a0a608a195f
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-20 23:25:32 -0500

    Declared attribute min which usses the collections method of min to read established array for minimum value.

--- MinMax/Array.java
+++ MinMax/Array.java
@@ -1,8 +1,13 @@
 package MinMax;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Array {
     public static void main(String[] args) {
         Integer[] num1 = {7,3,5,2,8};
+
+        int min = (int) Collections.min(Arrays.asList(num1));
 
     }
 }
