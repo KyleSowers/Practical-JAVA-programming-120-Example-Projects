051a3fc9a06f4ca6bede1a9e308fa6b5923fd92c
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-20 23:12:50 -0500

    Added 5 add methods to new ArrayList. Each with a character value.

--- ReverseArray/Array.java
+++ ReverseArray/Array.java
@@ -5,5 +5,10 @@ import java.util.ArrayList;
 public class Array {
     public static void main(String[] args) {
         ArrayList al = new ArrayList();
+        al.add("E");
+        al.add("J");
+        al.add("I");
+        al.add("K");
+        al.add("E");
     }
 }
