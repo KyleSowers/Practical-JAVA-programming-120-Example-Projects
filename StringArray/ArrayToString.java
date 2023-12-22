8544a25f8e003ecd750943989c99a558d2fd3931
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-22 14:10:49 -0500

    Added line comments explaing code.

--- StringArray/ArrayToString.java
+++ StringArray/ArrayToString.java
@@ -4,7 +4,9 @@ import java.util.*;
 
 public class ArrayToString {
     public static void main(String args[]) {
+        //declare new array stored in attribute x
         int[] x = {9, 5, 40, 33, 17, 2, 0};
+        //print x using the Arrays class, using the toString method
         System.out.println(Arrays.toString(x));
     }
 }
