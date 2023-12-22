e9bd65df3af3eb22a7ac647c609934077b17de58
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-22 14:08:48 -0500

    Declared new array with attribute x that has associated array values. Then used a print statement that calls on the Arrays method from java.util package, with the toString methodto print x array as a string.

--- StringArray/ArrayToString.java
+++ StringArray/ArrayToString.java
@@ -1,7 +1,10 @@
 package StringArray;
 
+import java.util.*;
+
 public class ArrayToString {
     public static void main(String args[]) {
-
+        int[] x = {9, 5, 40, 33, 17, 2, 0};
+        System.out.println(Arrays.toString(x));
     }
 }
