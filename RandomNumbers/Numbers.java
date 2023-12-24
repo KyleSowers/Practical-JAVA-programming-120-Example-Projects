adbe2564f0bcc92a632e93bbf8ce2727a11e244a
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-24 10:22:40 -0500

    Added a for loop that begins at value of one, with a condition that stops at/includes value of seven using a post increment. The print statement adds to the previous string print statement with the value results from the loop.

--- RandomNumbers/Numbers.java
+++ RandomNumbers/Numbers.java
@@ -5,8 +5,12 @@ import java.util.Random;
 public class Numbers {
     public static void main(String[] args) {
         int rand;
-        Random random = new Random();
+        Random radnum = new Random();
 
         System.out.println("The result of the random numbers are: ");
+
+        for ( rand = 1; rand <= 7; rand++) {
+            System.out.print(radnum.nextInt(2000) + "\n");
+        }
     }
 }
