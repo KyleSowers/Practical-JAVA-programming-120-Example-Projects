f58ab2026bbf893fe260e54b41fd1c367d83d520
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-23 23:36:33 -0500

    Made a for loop that does not define values within the initializer, condition, or post method creating no breakpoint value for loop. Then printing a statement every iteration, causing an infinite loop of printing.

--- InfiniteLoop/Loop.java
+++ InfiniteLoop/Loop.java
@@ -2,6 +2,8 @@ package InfiniteLoop;
 
 public class Loop {
     public static void main(String[] args) {
-
+        for (;;) {
+            System.out.println("This is an infinite loop");
+        }
     }
 }
