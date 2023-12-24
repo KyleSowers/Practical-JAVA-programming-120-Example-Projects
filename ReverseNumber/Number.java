2ee500bb698792b5432fe5d4d245e11a98f3e4c7
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-24 10:44:31 -0500

    Added a while loop that comletes the factoring for reversal through levels of arithmatic, based on the condition that the value does not equal 0.

--- ReverseNumber/Number.java
+++ ReverseNumber/Number.java
@@ -11,5 +11,11 @@ public class Number {
         Scanner sc = new Scanner(System.in);
 
         num2 = num1 = sc.nextInt();
+
+        while (num1 != 0) {
+            reversenum1 = reversenum1 * 10;
+            reversenum1 = reversenum1 + num1 % 10;
+            num1 = num1 / 10;
+        }
     }
 }
