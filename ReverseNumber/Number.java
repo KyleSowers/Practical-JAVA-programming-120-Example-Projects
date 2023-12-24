6dcf745aa55affb5ebf9cb97ee6f0bb525d60189
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-24 10:37:26 -0500

    Print statement requesting user for numbers.

--- ReverseNumber/Number.java
+++ ReverseNumber/Number.java
@@ -4,5 +4,7 @@ package ReverseNumber;
 public class Number {
     public static void main(String[] args) {
         int num1 = 0, num2, reversenum1 = 0;
+
+        System.out.println("Please insert some numbers: ");
     }
 }
