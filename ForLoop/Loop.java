de80aa6a805f7440a19f749ce26007efa00f3b28
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-23 23:26:22 -0500

    Added for loop where x starts at value of 10, where as long as x is greater then or equal to 1 it will print value of x with a statement, ending with a decrement before returing to next iteration of loop.'

--- ForLoop/Loop.java
+++ ForLoop/Loop.java
@@ -4,5 +4,9 @@ package ForLoop;
 public class Loop {
     public static void main(String[] args) {
         int x, y;
+
+        for (x = 10; x >= 1; x--) {
+            System.out.println("Value of x is: " + x);
+        }
     }
 }
