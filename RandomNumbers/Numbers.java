f5bad7f91d18c1287402f4b3f38188c3f1aa4765
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-24 10:17:27 -0500

    Print statement with initial string statement.

--- RandomNumbers/Numbers.java
+++ RandomNumbers/Numbers.java
@@ -6,5 +6,7 @@ public class Numbers {
     public static void main(String[] args) {
         int rand;
         Random random = new Random();
+
+        System.out.println("The result of the random numbers are: ");
     }
 }
