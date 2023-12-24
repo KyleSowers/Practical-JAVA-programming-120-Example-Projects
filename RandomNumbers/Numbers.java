697855f0ed6ce5e5879b4fd4dd340c7393aa5165
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-24 10:29:27 -0500

    Added line comments.

--- RandomNumbers/Numbers.java
+++ RandomNumbers/Numbers.java
@@ -5,12 +5,15 @@ import java.util.Random;
 
 public class Numbers {
     public static void main(String[] args) {
+        //declare new instance variable (attribute)
         int rand;
+        //create new random object with imported random class
         Random radnum = new Random();
-
+        //print initial output of string message
         System.out.println("The result of the random numbers are: ");
-
+        //loop for creating desired amount of random numbers
         for ( rand = 1; rand <= 7; rand++) {
+            //print random numbers
             System.out.print(radnum.nextInt(2000) + "\n");
         }
     }
