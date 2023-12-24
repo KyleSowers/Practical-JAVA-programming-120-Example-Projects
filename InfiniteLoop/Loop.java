a02e2187c1ca03e6a72b53ae085016090a1e0493
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-23 23:38:20 -0500

    Added line comments.

--- InfiniteLoop/Loop.java
+++ InfiniteLoop/Loop.java
@@ -2,7 +2,9 @@ package InfiniteLoop;
 
 public class Loop {
     public static void main(String[] args) {
+        //create a loop without an initial value, a condition, or post iteration method (no break point value)
         for (;;) {
+            //Statement will continue to print in an infinite looping event
             System.out.println("This is an infinite loop");
         }
     }
