3bfaf267be649c382a8bb1a60f19484b7f806bec
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-24 10:53:33 -0500

    Created new scanner object for class, from the inported scanner class.

--- FahrenheitToCentigrade/Convert.java
+++ FahrenheitToCentigrade/Convert.java
@@ -1,8 +1,12 @@
 package FahrenheitToCentigrade;
 
+import java.util.Scanner;
+
 public class Convert {
     public static void main(String[] args) {
         float radian;
         double degree;
+
+        Scanner sc = new Scanner(System.in);
     }
 }
