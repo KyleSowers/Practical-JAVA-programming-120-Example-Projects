fee104c0989af7367ff215e62045e03af6814049
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-24 10:38:29 -0500

    Added new Scanner with imported Scanner class.

--- ReverseNumber/Number.java
+++ ReverseNumber/Number.java
@@ -1,10 +1,13 @@
 //PRACTICE 56: A  PROGRAM TO REVERSE A NUMBER
 package ReverseNumber;
 
+import java.util.Scanner;
+
 public class Number {
     public static void main(String[] args) {
         int num1 = 0, num2, reversenum1 = 0;
 
         System.out.println("Please insert some numbers: ");
+        Scanner sc = new Scanner(System.in);
     }
 }
