0d85d121d91d8d3a6020511bd208968186280d15
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-24 10:15:24 -0500

    Imported random class, and created a new random object.

--- RandomNumbers/Numbers.java
+++ RandomNumbers/Numbers.java
@@ -1,7 +1,10 @@
 package RandomNumbers;
 
+import java.util.Random;
+
 public class Numbers {
     public static void main(String[] args) {
         int rand;
+        Random random = new Random();
     }
 }
