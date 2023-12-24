cacc613f97d6523b3fe2f1e257515f2de631bdb6
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-24 11:23:27 -0500

    Added line comments

--- FahrenheitToCentigrade/Convert.java
+++ FahrenheitToCentigrade/Convert.java
@@ -4,16 +4,18 @@ import java.util.Scanner;
 
 public class Convert {
     public static void main(String[] args) {
+        //Declared attributes
         float radian;
         double degree;
-
+        //Create new scanner
         Scanner sc = new Scanner(System.in);
-
+        //print request for value
         System.out.println("Please insert value in Fahrenheit: ");
+        //store value
         radian = sc.nextFloat();
-
+        //arithmatic for coversion
         degree = (radian - 32) / 1.8;
-
+        //print result
         System.out.println("Celsius equivalent of: " + radian + " Radian is " + degree + " Degree");
     }
 }
