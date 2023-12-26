155775fa47071be065ce90d5c2c0f60f51e57070
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-25 21:14:41 -0500

    A nested for loop, both levels with if conditionals was established to gain result of printing prime numbers.

--- PrimeNumbers/Prime.java
+++ PrimeNumbers/Prime.java
@@ -12,5 +12,17 @@ public class Prime {
         Scanner sc = new Scanner(System.in);
 
         int num2 = sc.nextInt();
+
+        for (x = 1; x <= num2; x++) {
+            int primecount = 0;
+            for (x = 1; num1 >= 1; num1--) {
+                if (x % num1 == 0) {
+                    primecount = primecount + 1;
+                }
+            }
+            if (primecount == 2) {
+                System.out.println("Prime number is " + x);
+            }
+        }
     }
 }
