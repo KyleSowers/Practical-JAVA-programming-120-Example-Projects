405d7e8470c75292fb648684cda11196a4d5591d
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-25 20:49:18 -0500

    Added line comments for code.

--- ReverseNumber/Reverse.java
+++ ReverseNumber/Reverse.java
@@ -3,18 +3,21 @@ package ReverseNumber;
 import java.util.*;
 public class Reverse {
     public static void main(String[] agrs) {
-
+        //declare attributes
         int num1 = 0, num2, reversenum1 = 0;
-
+        //request numbers from user
         System.out.println("Please insert different numbers");
+        //create scanner
         Scanner sc = new Scanner(System.in);
+        //read and store user value
         num2 = num1 - sc.nextInt();
-
+        //loop and solve for reverse number
         for (; num1 !=0; ) {
             reversenum1 = reversenum1 * 10;
             reversenum1 = reversenum1 + num1 % 10;
             num1 = num1 / 10;
         }
+        //provide result
         System.out.println("The reversed result of " + num2 + " is: " + reversenum1);
     }
 
