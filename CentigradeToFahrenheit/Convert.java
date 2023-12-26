6cb64afca4e76c87af64f977d2a56817d1697323
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-25 20:23:01 -0500

    Created new scanner imported from the Scanner class to read user input.

--- CentigradeToFahrenheit/Convert.java
+++ CentigradeToFahrenheit/Convert.java
@@ -1,10 +1,14 @@
 //PRACTICE 58: A PROGRAM TO CONVERT CENTIGRADE TO FAHRENHEIT
 package CentigradeToFahrenheit;
 
+import java.util.Scanner;
+
 public class Convert {
     public static void main(String[] args) {
 
         float degreeF;
         double degreeC;
+
+        Scanner sc = new Scanner(System.in);
     }
 }
