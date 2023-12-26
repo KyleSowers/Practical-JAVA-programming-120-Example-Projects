41cbc601fc54d4cbf98838ba38ad0ed09e1c39c8
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-25 21:16:17 -0500

    Added line comments.

--- PrimeNumbers/Prime.java
+++ PrimeNumbers/Prime.java
@@ -4,15 +4,16 @@ import java.util.Scanner;
 
 public class Prime {
     public static void main(String[] args) {
-
+        //declare attributes
         int x = 0, num1 = 0;
         String pnum = "";
-
+        //request a number from user
         System.out.println("Insert a number to print its prime numbers: ");
+        //create new scanner
         Scanner sc = new Scanner(System.in);
-
+        //use new scanner to read and store input
         int num2 = sc.nextInt();
-
+        //nested for loop with both levels of if conditionals to determine and print out print numbers
         for (x = 1; x <= num2; x++) {
             int primecount = 0;
             for (x = 1; num1 >= 1; num1--) {
