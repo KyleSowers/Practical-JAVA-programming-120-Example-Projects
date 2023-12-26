666711098ebb1dd3a3322f61135df1ffa7c1a94c
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-25 20:37:06 -0500

    Added missing main maethod to class.

--- ReverseNumber/Reverse.java
+++ ReverseNumber/Reverse.java
@@ -1,7 +1,10 @@
 //PRACTICE 59: A PROGRAM TO REVERSE A NUMBER USING FOR LOOP
 package ReverseNumber;
-
+import java.util.*;
 public class Reverse {
+    public static void main(String[] agrs) {
 
-    int num1 = 0, num2, reversenum1 = 0;
+        int num1 = 0, num2;
+    }
+
 }
