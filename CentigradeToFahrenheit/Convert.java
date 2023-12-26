8c24b07078637a54ee03979ba00d1bafc9bd1e27
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-25 20:30:04 -0500

    Added line comments.

--- CentigradeToFahrenheit/Convert.java
+++ CentigradeToFahrenheit/Convert.java
@@ -5,17 +5,18 @@ import java.util.Scanner;
 
 public class Convert {
     public static void main(String[] args) {
-
+        //declare attributes to use
         float degreeF;
         double degreeC;
-
+        //create new scanner
         Scanner sc = new Scanner(System.in);
-
+        //request value from user
         System.out.println("Please insert values in Celsius");
+        //read and store value
         degreeF = sc.nextFloat();
-
+        //convert value and store result
         degreeC = (1.8 * degreeF) + 32;
-
+        //print results
         System.out.println("Celsius equivalent of " + degreeF + " is: " + degreeC);
     }
 }
