27dab94069b07f155401c8538e10f8daac40f72f
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-26 22:31:20 -0500

    Added line comments.

--- helloWorld/currentdate.java
+++ helloWorld/currentdate.java
@@ -6,14 +6,14 @@ import java.util.Date;
 
 public class currentdate {
     public static void main(String[] args) {
-
+        //create new Date object from the imported Date class
         Date dTime = new Date();
-
+        //create independent SimpleDateFormat objects each with different date and time value results
         SimpleDateFormat sdf1 = new SimpleDateFormat("dd/MM/yyyy");
         SimpleDateFormat sdf2 = new SimpleDateFormat("E");
         SimpleDateFormat sdf3 = new SimpleDateFormat("hh:mm:ss a");
         SimpleDateFormat sdf4 = new SimpleDateFormat("MMM");
-
+        //print each SimpleDateFormat object value from the Date object
         System.out.println("Current Date is: " + sdf1.format(dTime));
         System.out.println("Current Day is: " + sdf2.format(dTime));
         System.out.println("Current Time is: " + sdf3.format(dTime));
