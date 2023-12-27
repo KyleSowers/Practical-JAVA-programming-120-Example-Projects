8b168415c4a95ab6ee695b6434e8d26729e0edfe
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-26 22:16:30 -0500

    Added line comments explaining code.

--- SwapNumber2/Swap2.java
+++ SwapNumber2/Swap2.java
@@ -2,19 +2,20 @@ package SwapNumber2;
 
 public class Swap2 {
     public static void main(String[] args) {
+        //declare attributes with values
         int x = 200;
         int y = 500;
-
+        //print before swap values
         System.out.println("Values before swapping");
         System.out.println("Value of x is: " + x);
         System.out.println("Value of y is: " + y);
-
+        //arithmatic to create swap
         x = x + y;
         y = x - y;
         x = x - y;
-
+        //create new line
         System.out.println("\n");
-
+        //print result of swapped attribute values
         System.out.println("Values after swapping");
         System.out.println("Value of x is: " + x);
         System.out.println("Value of y is: " + y);
