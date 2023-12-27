4add0f934e659dc692db68a12eff471e2a4ddf97
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-26 22:25:05 -0500

    Created 4 SimpleDateFormat objects each with different values for different results.

--- helloWorld/currentdate.java
+++ helloWorld/currentdate.java
@@ -1,10 +1,17 @@
 package helloWorld;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class currentdate {
     public static void main(String[] args) {
 
         Date dTime = new Date();
+
+        SimpleDateFormat sdf1 = new SimpleDateFormat("dd/MM/yyyy");
+        SimpleDateFormat sdf2 = new SimpleDateFormat("E");
+        SimpleDateFormat sdf3 = new SimpleDateFormat("hh:mm:ss a");
+        SimpleDateFormat sdf4 = new SimpleDateFormat("MMM");
+
     }
 }
