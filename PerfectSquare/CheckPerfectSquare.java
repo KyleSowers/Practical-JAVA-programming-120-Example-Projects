02657353dad78af3478ad1b48dc3fae922f0fb7d
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-27 10:18:39 -0500

    Created new scanner for class imported from java scanner class.

--- PerfectSquare/CheckPerfectSquare.java
+++ PerfectSquare/CheckPerfectSquare.java
@@ -1,8 +1,11 @@
 //PRACTICE 66: A PROGRAM TO CHECK A PERFECT SQUARE
 package PerfectSquare;
 
+import java.util.Scanner;
+
 public class CheckPerfectSquare {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
     }
 }
