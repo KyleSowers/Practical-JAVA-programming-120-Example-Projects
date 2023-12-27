d09d4c4d28516f61dbceb43a75d9c544ac625980
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-26 22:08:57 -0500

    Added print statements providing user with results before swapping method takes place.

--- SwapNumber2/Swap2.java
+++ SwapNumber2/Swap2.java
@@ -5,5 +5,9 @@ public class Swap2 {
         int x = 200;
         int y = 500;
 
+        System.out.println("Values before swapping");
+        System.out.println("Value of x is: " + x);
+        System.out.println("Value of y is: " + y);
+
     }
 }
