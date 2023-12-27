9b7598b0f9e5833421ea0c7d591168d19e8b1d98
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-27 10:27:02 -0500

    Added line comments.

--- PerfectSquare/CheckPerfectSquare.java
+++ PerfectSquare/CheckPerfectSquare.java
@@ -5,13 +5,15 @@ import java.util.Scanner;
 
 public class CheckPerfectSquare {
     public static void main(String[] args) {
-
+        //add new scanner to class
         Scanner sc = new Scanner(System.in);
-
+        //ask user for a number
         System.out.println("Please, insert a number: ");
+        //store user number into attribute
         int x = sc.nextInt();
-
+        //factor sqrt and store into new attribute
         int sqrt = (int)Math.sqrt(x);
+        //condition to check for perfect square or not with print statements
         if (sqrt * sqrt == x) {
             System.out.println(x + " is a Perfect Square");
         } else {
