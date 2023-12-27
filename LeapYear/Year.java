d9e663ff847ff9abca839e173c3b21b5381f551d
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-27 09:49:12 -0500

    Added for loop that checks if the value of the attribute from user input is evenly divisible by 400 OR is evenly divisible by 4 but not 100, which would print out result that it is a leap year. Otherwise if the condition is not met it will pass to the else statement that prints out it is not a leap year.

--- LeapYear/Year.java
+++ LeapYear/Year.java
@@ -10,5 +10,11 @@ public class Year {
         Scanner sc = new Scanner(System.in);
 
         int yr = sc.nextInt();
+
+        if ((yr % 400 == 0) || ((yr % 4 == 0) && (yr % 100 != 0))) {
+            System.out.println("This year : " + yr + " is a leap year");
+        } else {
+            System.out.println("This year " + yr + " is not a leap year");
+        }
     }
 }
