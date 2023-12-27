f169244fccc57e77f43fc13df5a15caebbf02411
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-27 09:51:22 -0500

    Added line comments about practice.

--- LeapYear/Year.java
+++ LeapYear/Year.java
@@ -5,12 +5,13 @@ import java.util.Scanner;
 
 public class Year {
     public static void  main(String[] args) {
-
+        //ask user for a year
         System.out.println("Please insert a preferred year: ");
+        //create new scanner to read user input
         Scanner sc = new Scanner(System.in);
-
+        //declare new variable and store input value
         int yr = sc.nextInt();
-
+        //using modulus/operators determine if leap year and print correct result
         if ((yr % 400 == 0) || ((yr % 4 == 0) && (yr % 100 != 0))) {
             System.out.println("This year : " + yr + " is a leap year");
         } else {
