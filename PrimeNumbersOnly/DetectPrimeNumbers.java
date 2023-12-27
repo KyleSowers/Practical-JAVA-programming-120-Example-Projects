71c5725c4e3825314f7b4d217296bc17f6506b94
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-27 09:57:27 -0500

    Created new scanner for class imported from the scannner class.

--- PrimeNumbersOnly/DetectPrimeNumbers.java
+++ PrimeNumbersOnly/DetectPrimeNumbers.java
@@ -1,11 +1,13 @@
 //PRACTICE 65: A PROGRAM TO DETECT PRIME NUMBERS ONLY
 package PrimeNumbersOnly;
 
+import java.util.Scanner;
+
 public class DetectPrimeNumbers {
     public static void main(String[] args) {
 
         int num1, x, count = 0;
 
-
+        Scanner sc = new Scanner(System.in);
     }
 }
