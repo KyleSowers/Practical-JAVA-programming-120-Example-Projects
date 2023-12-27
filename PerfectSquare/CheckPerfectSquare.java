98ffbd7106ba289a0b9e2c3723b043b1dbcad703
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-27 10:19:20 -0500

    Request a number from the user.

--- PerfectSquare/CheckPerfectSquare.java
+++ PerfectSquare/CheckPerfectSquare.java
@@ -7,5 +7,7 @@ public class CheckPerfectSquare {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please, insert a number: ");
     }
 }
