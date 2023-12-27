932d7812b3a89eae4dd4968014daa1ba3cfac848
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-27 09:42:10 -0500

    Added new scanner from the imported scanner class.

--- LeapYear/Year.java
+++ LeapYear/Year.java
@@ -1,9 +1,12 @@
 //PRACTICE 64: A PROGRAM TO DETECT A LEAP YEAR
 package LeapYear;
 
+import java.util.Scanner;
+
 public class Year {
     public static void  main(String[] args) {
 
         System.out.println("Please insert a preferred year: ");
+        Scanner sc = new Scanner(System.in);
     }
 }
