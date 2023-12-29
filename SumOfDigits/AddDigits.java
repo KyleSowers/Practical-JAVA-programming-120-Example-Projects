ec10688af7a0b73524b42d162edb9975dc72ebe6
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-28 21:12:54 -0500

    Created new scanner with import of the java.util.scanner class.

--- SumOfDigits/AddDigits.java
+++ SumOfDigits/AddDigits.java
@@ -1,7 +1,10 @@
 package SumOfDigits;
 
+import java.util.Scanner;
+
 public class AddDigits {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
     }
 }
