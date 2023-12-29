47b64b5401d00ca58e98a1c35ecd5ba6aa377082
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-29 08:23:18 -0500

    Added line comments.

--- ArmstrongNumber/Armstrong.java
+++ ArmstrongNumber/Armstrong.java
@@ -6,19 +6,23 @@ import java.util.Scanner;
 public class Armstrong {
 
     public static void main(String[] args) {
+        //declare new int attributes
         int x, y, z = 0, ams;
+        //create new class scanner
         Scanner sc = new Scanner(System.in);
-
+        //request positive numbers from user
         System.out.println("Please insert only positive numbers: ");
-
+        //read and store user input
         x = sc.nextInt();
+        //store user value into second attribute through first attribute stored value
         y = x;
-
+        //while loop will factor the attributes
         while (y != 0) {
             ams = y % 10;
             z = z + ams * ams * ams;
             y = y / 10;
         }
+        //if else statement will check and share result for Armstrong Number
         if (z == x) {
             System.out.println(x + " is an Armstrong Number");
         } else {
