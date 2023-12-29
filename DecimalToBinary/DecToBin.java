70ade167f2576d2c2574a6988d9763769bf909e5
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-28 20:41:12 -0500

    Declared both an integer array with a size of 40 and int attribute with value of 0.

--- DecimalToBinary/DecToBin.java
+++ DecimalToBinary/DecToBin.java
@@ -3,7 +3,8 @@ package DecimalToBinary;
 
 public class DecToBin {
     public void Convert(int num1) {
-
+        int bin[] = new int[40];
+        int ind = 0
     }
 
     public static void  main(String[] args) {
