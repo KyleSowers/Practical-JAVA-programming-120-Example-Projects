646a21b6a29a80c119028ecaca3d9996e9f2a4e3
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-29 08:05:48 -0500

    Print statement requesting users to add only positive numbers.

--- ArmstrongNumber/Armstrong.java
+++ ArmstrongNumber/Armstrong.java
@@ -7,5 +7,7 @@ public class Armstrong {
     public static void main(String[] args) {
         int x, y, z = 0, ams;
         Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please insert only positive numbers: ");
     }
 }
