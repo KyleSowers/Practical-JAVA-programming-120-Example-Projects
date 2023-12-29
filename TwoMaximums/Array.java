e3386b15579fbbcffafd207f4bb19ea92ed69057
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-28 21:30:11 -0500

    Made new Array object.

--- TwoMaximums/Array.java
+++ TwoMaximums/Array.java
@@ -5,6 +5,6 @@ public class Array {
     public static void  main(String[] args) {
         int num2[] = {4300, 78, 9, 34, 100, 5000, 432, 678, 12, 7, 3, 0, 654};
 
-
+        Array ay = new Array();
     }
 }
