f41832a61c786bf551cc5ac10fb9b79589a68164
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-28 21:29:23 -0500

    Declared new int array with stroed values.

--- TwoMaximums/Array.java
+++ TwoMaximums/Array.java
@@ -3,6 +3,8 @@ package TwoMaximums;
 
 public class Array {
     public static void  main(String[] args) {
+        int num2[] = {4300, 78, 9, 34, 100, 5000, 432, 678, 12, 7, 3, 0, 654};
+
 
     }
 }
