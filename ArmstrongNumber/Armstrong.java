3611e4fdd013e92517423c47be10a3060c89665d
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-29 08:04:52 -0500

    Created new scanner for class inported from the java.util.scanner class.

--- ArmstrongNumber/Armstrong.java
+++ ArmstrongNumber/Armstrong.java
@@ -1,8 +1,11 @@
 package ArmstrongNumber;
 
+import java.util.Scanner;
+
 public class Armstrong {
 
     public static void main(String[] args) {
         int x, y, z = 0, ams;
+        Scanner sc = new Scanner(System.in);
     }
 }
