d79b6c222fe4a50ba0940ee6518f32b12e4756f8
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-29 07:41:43 -0500

    To correct branch, main method was added, just added String attribute with a initial value of false.

--- StringToDataTypes/ConvertString.java
+++ StringToDataTypes/ConvertString.java
@@ -1,8 +1,10 @@
+//PRACTICE 71: A PROGRAM TO CONVERT FROM STRING TO OTHER DATA TYPES
 package StringToDataTypes;
 
 public class ConvertString {
 
     public static void main(String[] args) {
 
+        String Str1 = "False";
     }
 }
