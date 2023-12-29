ef0e5f1584cc2a83a80ae0e24148edcf02e0f021
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-28 21:13:41 -0500

    Printed request to user for numbers.

--- SumOfDigits/AddDigits.java
+++ SumOfDigits/AddDigits.java
@@ -6,5 +6,6 @@ public class AddDigits {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        System.out.println("Please insert some numbers: ");
     }
 }
