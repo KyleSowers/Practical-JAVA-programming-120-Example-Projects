836ef17c825f2b6223d8cd0915604d2b05e7f116
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-29 07:37:08 -0500

    Added line comments to explain code.

--- StirngToCharacter/Convert.java
+++ StirngToCharacter/Convert.java
@@ -3,13 +3,13 @@ package StirngToCharacter;
 
 public class Convert {
     public static void main(String[] args) {
-
+        //declare string attribute
         String helloStr = "This is the original String object";
-
+        //declare new char array
         char[] StrArray;
-
+        //add string attribute, separate each word of string into elements in array
         StrArray = helloStr.toCharArray();
-
+        //print result to user, after looping through array for each char element
         System.out.println("The result of the character array is below: ");
         for (int x = 0; x < StrArray.length; x++) {
             System.out.print(StrArray[x]);
