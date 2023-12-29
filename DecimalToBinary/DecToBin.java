d8468f5d2a5d4d0f782059f81447f0c4c8bcad16
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-28 20:48:12 -0500

    Added while arguement is greater than 0, array index is stored with value of remainder of arguement divided by two, per index, then arguement attribute stores value of itself divided by two. The for loop iterates through the bin in reverse order before printing.

--- DecimalToBinary/DecToBin.java
+++ DecimalToBinary/DecToBin.java
@@ -4,7 +4,15 @@ package DecimalToBinary;
 public class DecToBin {
     public void Convert(int num1) {
         int bin[] = new int[40];
-        int ind = 0
+        int ind = 0;
+
+        while (num1 > 0) {
+            bin[ind++] = num1 % 2;
+            num1 = num1 / 2;
+        }
+        for (int x = ind - 1; x >= 0; x--) {
+            System.out.println(bin[x]);
+        }
     }
 
     public static void  main(String[] args) {
