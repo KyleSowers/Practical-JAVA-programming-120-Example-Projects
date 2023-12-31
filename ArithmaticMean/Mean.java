6341ce0e6400c82d2d1577f57f9338569ed6120e
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-30 18:25:35 -0600

    Created new scanner for class.

--- ArithmaticMean/Mean.java
+++ ArithmaticMean/Mean.java
@@ -1,9 +1,13 @@
 package ArithmaticMean;
 
+import java.util.Scanner;
+
 public class Mean {
     public static void main(String[] args) {
 
         int x, y, add = 0, meannum;
         int num1[] = new int[50];
+
+        Scanner sc = new Scanner(System.in);
     }
 }
