2e73d4bd8c668fea28775168fe1b8e4b7d82e725
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-31 21:03:39 -0600

    Created new scanner for the class.

--- DecimalToOctal/Convert.java
+++ DecimalToOctal/Convert.java
@@ -1,10 +1,14 @@
 //PRACTICE 75: A PROGRAM TO CONVERT FROM DECIMAL TO OCTAL
 package DecimalToOctal;
 
+import java.util.Scanner;
+
 public class Convert {
     public static void main(String[] args) {
 
         int DNum, x = 1, y, num1;
         int ONum[] = new int[100];
+
+        Scanner sc = new Scanner(System.in);
     }
 }
