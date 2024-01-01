e1b60aea031991d205cf548900158d0317fbd4aa
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-12-31 21:26:42 -0600

    Created new scanner for the class.

--- BinaryToDecimal/Convert.java
+++ BinaryToDecimal/Convert.java
@@ -1,9 +1,12 @@
 //PRACTICE 76: A PROGRAM TO CONVERT FROM BINARY TO DECIMAL
 package BinaryToDecimal;
 
+import java.util.Scanner;
+
 public class Convert {
     public static void main(String[] args) {
 
         int BNum, DNum = 0, x = 1, num1;
+        Scanner sc = new Scanner(System.in);
     }
 }
