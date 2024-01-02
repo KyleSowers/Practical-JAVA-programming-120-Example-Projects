b29a74a570b85fc40c0e8b2b70dd526bc376d800
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-01 21:09:23 -0600

    Added line comments for code.

--- CurrentLocale/Current.java
+++ CurrentLocale/Current.java
@@ -5,19 +5,19 @@ import java.util.Locale;
 
 public class Current {
     public static void main(String[] args) {
-
+        //create a Locale for class
         Locale lc = Locale.getDefault();
-
+        //print country and language for user
         System.out.println(lc.getDisplayCountry());
         System.out.println(lc.getDisplayLanguage());
-
+        //new line or space
         System.out.println("\n");
-
+        //print country and language...abbreviation result
         System.out.println(lc.getCountry());
         System.out.println(lc.getLanguage());
-
+        //new line or space
         System.out.println("\n");
-
+        //another method for printing abbreviated country and language
         System.out.println(System.getProperty("user.country"));
         System.out.println(System.getProperty("user.language"));
     }
