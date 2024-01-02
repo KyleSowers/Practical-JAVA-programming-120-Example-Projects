8154842e949d19220f1281b74e6ae8ca98b0d6c1
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-02 14:00:45 -0600

    Initiated new hashmap where the keys and vlaues ae both string, imported map and hashmap classes.

--- helloWorld/HashMap1.java
+++ helloWorld/HashMap1.java
@@ -1,9 +1,12 @@
 //PRACTICE 80: A PROGRAM TO ITERATE THROUGH HASHMAP
 package helloWorld;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HashMap1 {
     public static void main(String[] args) {
 
-
+    Map<String, String> mp = new HashMap<String, String>();
     }
 }
