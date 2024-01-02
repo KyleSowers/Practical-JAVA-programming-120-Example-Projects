b76e021611483e87298b398eec5f0c5eaaeb2709
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-01 20:57:53 -0600

    Created new Locale imported from the java.util.Locale class.

--- CurrentLocale/Current.java
+++ CurrentLocale/Current.java
@@ -1,8 +1,11 @@
 //PRACTICE 78: A PROGRAM TO CREATE CURRENT LOCALE
 package CurrentLocale;
 
+import java.util.Locale;
+
 public class Current {
     public static void main(String[] args) {
 
+        Locale lc = Locale.getDefault();
     }
 }
