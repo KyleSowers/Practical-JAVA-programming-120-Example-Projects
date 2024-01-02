376a917478fa71d663e0731481338647c0f80ea7
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-01 20:44:49 -0600

    Declared new char array with values from 0-9 and A-F.

--- DecimalToHexadecimal/Convert.java
+++ DecimalToHexadecimal/Convert.java
@@ -6,5 +6,9 @@ public class Convert {
     public static void  main(String[] args) {
         int DNum, num1;
         String HNum = "";
+
+        char Hex[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+
+
     }
 }
