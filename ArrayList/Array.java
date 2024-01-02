f6de0374f543ca49874ce4b33a946289ef154fda
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-01 21:14:03 -0600

    Added new ArrayList with import of java.util.ArrayList.

--- ArrayList/Array.java
+++ ArrayList/Array.java
@@ -1,9 +1,11 @@
 //PRACTICE 79: A PROGRAM FOR ARRAY LIST
 package ArrayList;
 
+import java.util.ArrayList;
+
 public class Array {
     public static void main(String[] args) {
 
-
+        ArrayList<String> List = new ArrayList<>();
     }
 }
