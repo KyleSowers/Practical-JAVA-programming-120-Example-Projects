ec5f1cc86483c829536f5d219718f29ef34991e0
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-02 14:29:11 -0600

    Print statement for showing map before sorting.

--- helloWorld/hello1.java
+++ helloWorld/hello1.java
@@ -2,11 +2,22 @@
 package helloWorld;
 
 import java.util.HashMap;
+import java.util.Set;
 
 public class hello1 {
 
     public static void main(String[] args) {
 
         HashMap<String, Integer> mp = new HashMap();
+
+        mp.put("g Saturday", 6);
+        mp.put("e, Thursday", 4);
+        mp.put("b, Tuesday", 2);
+        mp.put("f, Friday", 5);
+        mp.put("h, Sunday", 7);
+        mp.put("c, Wednesday", 3);
+        mp.put("a, Monday", 1);
+
+        System.out.println("Maps before sorting: ");
     }
 }
