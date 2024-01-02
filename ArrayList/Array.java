f48797109c60579eecda3cf6f96908fa4c47f9d6
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-01 21:15:35 -0600

    Added list items USA, Canada, Nigeria, and China.

--- ArrayList/Array.java
+++ ArrayList/Array.java
@@ -6,6 +6,11 @@ import java.util.ArrayList;
 public class Array {
     public static void main(String[] args) {
 
-        ArrayList<String> List = new ArrayList<>();
+        ArrayList<String> list = new ArrayList<>();
+
+        list.add("USA");
+        list.add("Canada");
+        list.add("Nigeria");
+        list.add("China");
     }
 }
