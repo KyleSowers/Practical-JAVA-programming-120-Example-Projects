6fb3dbd623c15ce46da67ffaeee2533af800d259
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-02 14:22:16 -0600

    Created new hashmap that is a key/value pair of string/integer.

--- helloWorld/hello1.java
+++ helloWorld/hello1.java
@@ -1,9 +1,12 @@
 //PRACTICE 81: A PROGRAM TO SORT HASHMAP
 package helloWorld;
 
+import java.util.HashMap;
+
 public class hello1 {
 
     public static void main(String[] args) {
 
+        HashMap<String, Integer> mp = new HashMap();
     }
 }
