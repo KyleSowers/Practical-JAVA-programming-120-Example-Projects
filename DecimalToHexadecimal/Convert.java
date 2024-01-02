4056bc1fdb047bbd931ec91e17f60756485a25d2
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-01 20:46:23 -0600

    A print statement asks the user for an input.

--- DecimalToHexadecimal/Convert.java
+++ DecimalToHexadecimal/Convert.java
@@ -12,5 +12,7 @@ public class Convert {
         char Hex[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
 
         Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please, insert Decimal number: ");
     }
 }
