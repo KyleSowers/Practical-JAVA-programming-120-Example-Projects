b697c73c779390cf537dfaf3c124981eb5ba3c34
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-03 19:25:58 -0600

    Created new hashmap wih key value pair of integer and string. Hashmap class was inported.

--- SortHashMap/SortHashMap.java
+++ SortHashMap/SortHashMap.java
@@ -1,8 +1,11 @@
 //PRACTICE 82: A PROGRAM TO SORT HASHMAP BY VALUES
 package SortHashMap;
 
+import java.util.HashMap;
+
 public class SortHashMap {
     public static void main(String[] args) {
 
+        HashMap<Integer, String> hm = new HashMap<Integer, String>();
     }
 }
