c623033175c23c7b83b8ec31a0ee10ff2d3d85fd
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-03 19:28:32 -0600

    Used put method seven times to imput key value pair of the days of the week into the hashmap.

--- SortHashMap/SortHashMap.java
+++ SortHashMap/SortHashMap.java
@@ -7,5 +7,12 @@ public class SortHashMap {
     public static void main(String[] args) {
 
         HashMap<Integer, String> hm = new HashMap<Integer, String>();
+        hm.put(7, "Sunday");
+        hm.put(4, "Thursday");
+        hm.put(2, "Tuesday");
+        hm.put(5, "Friday");
+        hm.put(1, "Monday");
+        hm.put(6, "Saturday");
+        hm.put(3, "Wednesday");
     }
 }
