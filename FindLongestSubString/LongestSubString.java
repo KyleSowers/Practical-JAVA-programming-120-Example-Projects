b25dccd65752fc5d55bc9aa87eee6eee214996cb
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-03 20:14:38 -0600

    A for loop with attribute x at value 0, checks for x < str.length, which then declares a new char attribute and stores the char at element x.

--- FindLongestSubString/LongestSubString.java
+++ FindLongestSubString/LongestSubString.java
@@ -1,9 +1,13 @@
 package FindLongestSubString;
 
 public class LongestSubString {
-    private static String LongestStr(String str)
+    private static String LongestStr(String str) {
         String str1 = " ";
         String str2 = " ";
 
+        for (int x = 0; x < str.length(); x++) {
+            char ch = str.charAt(x);
 
+        }
+    }
 }
