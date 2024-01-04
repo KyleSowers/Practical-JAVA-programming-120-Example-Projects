1b08736e81815464dca2b65130cfb5de041fc8c0
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-03 20:23:58 -0600

    Fighting issues with commit. Loop checking for if ch exists, if not add and continue loop.

--- FindLongestSubString/LongestSubString.java
+++ FindLongestSubString/LongestSubString.java
@@ -2,12 +2,19 @@ package FindLongestSubString;
 
 public class LongestSubString {
     private static String LongestStr(String str) {
-        String str1 = " ";
-        String str2 = " ";
+        String str1 = "";
+        String str2 = "";
 
         for (int x = 0; x < str.length(); x++) {
             char ch = str.charAt(x);
-
+            if(str1.indexOf(ch) == -1) {
+                str1 += ch;
+                continue;
+            } else {
+                if (str1.length() > str2.length()) {
+                    str2 = str1;
+                }
+            }
         }
     }
 }
