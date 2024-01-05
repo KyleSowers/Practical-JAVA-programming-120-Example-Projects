242d687af8f369aae0ec2e7824bc24974e7ab2ff
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-04 18:55:00 -0600

    Added line comments.

--- EmployeesData/Employee.java
+++ EmployeesData/Employee.java
@@ -5,18 +5,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Employee {
-
+    //declare private attributes
     private String name;
     private String occupation;
     private int salary;
-
+    //constructor for class attributes
     public Employee(String firstname, String job, int value) {
         super();
         this.name = firstname;
         this.occupation = job;
         this.salary = value;
     }
-
+    //getters and setters of class attributes
     public String getName() {
         return name;
     }
@@ -40,20 +40,20 @@ public class Employee {
     public void setSalary(int salary) {
         this.salary = salary;
     }
-
+    //method to return string for use in upcoming array list
     public String toString() {
-        return "name is: " + name + " Occupatin is: " + occupation + " Salary is: " + salary;
+        return "name is: " + name + " Occupation is: " + occupation + " Salary is: " + salary;
     }
 
     public static void main(String[] args) {
-
+        //create employees
         Employee employee1 = new Employee("Hello","Programmer",1000);
         Employee employee2 = new Employee("Jude","Doctor",4000);
         Employee employee3 = new Employee("Joseph","Lawyer",500);
         Employee employee4 = new Employee("Vivian","Nurse",700);
         Employee employee5 = new Employee("Juliet","Chef",10000);
         Employee employee6 = new Employee("Mark","Gardener",70000);
-
+        //create new array list
         List<Employee> employeeList = new ArrayList();
         employeeList.add(employee1);
         employeeList.add(employee2);
@@ -61,7 +61,7 @@ public class Employee {
         employeeList.add(employee4);
         employeeList.add(employee5);
         employeeList.add(employee6);
-
+        //provide result, using simplified for-each
         System.out.println("The Employee data is below \n");
         for (Employee employee:employeeList) {
             System.out.println(employee);
