6ca9396f8456e53ac136fbacb67fa28b8d1875fb
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-05 19:17:53 -0600

    Initialized new HashMap that accepts a character followed by integer.

--- FirstNonRepeatedCharacter/NonRepeated.java
+++ FirstNonRepeatedCharacter/NonRepeated.java
@@ -1,8 +1,11 @@
 //PRACTICE 87: A PROGRAM TO DETECT FIRST NON-REPEATED CHARACTER
 package FirstNonRepeatedCharacter;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class NonRepeated {
         public static void main(String[] args) {
-
+            Map<Character, Integer> chmap = new HashMap();
         }
 }
