66ca409bbc2e3380c5c15e2dd1e524c47b1d594d
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-05 18:55:39 -0600

    Per the implemented Comparator, an override and call addition of the compare method will take in two Employee parameters, then return the firsts salary - the seconds.

--- SortObjectList/sortObjects.java
+++ SortObjectList/sortObjects.java
@@ -4,4 +4,9 @@ package SortObjectList;
 import java.util.Comparator;
 
 public class sortObjects implements Comparator<Employee> {
+
+    @Override
+    public int compare(Employee employee1, Employee employee2) {
+        return(employee1.getSalary() - employee2.getSalary());
+    }
 }
