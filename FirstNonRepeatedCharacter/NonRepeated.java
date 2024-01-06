1a805f59fad4afc2a0cb9a3b17bc5f11c08a443e
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-05 19:18:32 -0600

    Created a new scanner for the class.

--- FirstNonRepeatedCharacter/NonRepeated.java
+++ FirstNonRepeatedCharacter/NonRepeated.java
@@ -3,9 +3,11 @@ package FirstNonRepeatedCharacter;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class NonRepeated {
         public static void main(String[] args) {
             Map<Character, Integer> chmap = new HashMap();
+            Scanner sc = new Scanner(System.in);
         }
 }
