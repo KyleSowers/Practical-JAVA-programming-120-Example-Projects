ef49710d08011cf476ac9bdb7a2be9e5b8f8eea8
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-05 18:56:32 -0600

    Added main method to class.

--- SortObjectList/sortObjects.java
+++ SortObjectList/sortObjects.java
@@ -9,4 +9,10 @@ public class sortObjects implements Comparator<Employee> {
     public int compare(Employee employee1, Employee employee2) {
         return(employee1.getSalary() - employee2.getSalary());
     }
+
+    public static void main(String[] args) {
+
+    }
 }
+
+
