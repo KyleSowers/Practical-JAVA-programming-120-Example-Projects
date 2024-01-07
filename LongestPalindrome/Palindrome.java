86a6980619189c31c4add7fbbbae3631c3f7de82
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-06 20:09:26 -0600

    Added LongestPlindrome method.

--- LongestPalindrome/Palindrome.java
+++ LongestPalindrome/Palindrome.java
@@ -11,15 +11,25 @@ public class Palindrome {
         }
 
         String lngs = Str1.substring(0, 1);
-        for(int x = 0; x < Str1.length(); x++) {
+        for (int x = 0; x < Str1.length(); x++) {
             String lngx = LongestPalindrome(Str1, x, x);
             if (lngx.length() > lngs.length()) {
-            lngs = lngx;
-        }
+                lngs = lngx;
+            }
 
-        lngx = LongestPalindrome(Str1, x, x + 1);
-        if(lngx.length() > lngs.length()) {
-            lngs = lngx;
+            lngx = LongestPalindrome(Str1, x, x + 1);
+            if (lngx.length() > lngs.length()) {
+                lngs = lngx;
+            }
         }
+        return lngs;
+    }
+
+    public String LongestPalindrome(String Str1, int start, int last) {
+        while(start >= 0 && last <= Str1.length() -1 && Str1.charAt(start) == Str1.charAt(last)) {
+            start--;
+            last++;
+        }
+        return Str1.substring(start +1, last);
     }
 }
