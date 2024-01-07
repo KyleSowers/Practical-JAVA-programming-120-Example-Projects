7cbb44787cdc8f26f82b1d4884cba7f33ef914b7
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-06 19:34:21 -0600

    Created new scanner for class.

--- StringPermutation/Permutation.java
+++ StringPermutation/Permutation.java
@@ -1,8 +1,10 @@
 //PRACTICE 88: A PROGRAM TO GENERATE PERMUTATION OF A STRING
 package StringPermutation;
 
+import java.util.Scanner;
+
 public class Permutation {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
     }
 }
