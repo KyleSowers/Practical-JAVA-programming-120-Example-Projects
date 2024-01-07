ddfb970e35f2f908ff1a47a8f74345c62cac57b5
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-06 19:35:11 -0600

    A print statement asking user for a string.

--- StringPermutation/Permutation.java
+++ StringPermutation/Permutation.java
@@ -6,5 +6,7 @@ import java.util.Scanner;
 public class Permutation {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please, insert a string: ");
     }
 }
