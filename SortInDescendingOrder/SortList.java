ae8f14a3504e79472b83c52231faf6e0b2419bd4
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-07 19:48:34 -0600

    Added line comments for code.

--- SortInDescendingOrder/SortList.java
+++ SortInDescendingOrder/SortList.java
@@ -6,9 +6,9 @@ import java.util.Collections;
 
 public class SortList {
     public static void main(String[] args) {
-
+        //create new arraylist
         ArrayList<String> al = new ArrayList<String>();
-
+        //add strings to arraylist
         al.add("KYC");
         al.add("EYE");
         al.add("HELLO");
@@ -17,17 +17,17 @@ public class SortList {
         al.add("AT");
         al.add("JAVA");
         al.add("PYTHON");
-
+        //show user arraylist before sorting method
         System.out.println("Array before sorting: ");
         for(String Str1: al) {
             System.out.println(Str1);
         }
-
+        //create new line before sorting
         System.out.println("\n");
-
+        //use collections class to use its sort method on arraylist and utilize the reverse order method
         Collections.sort(al, Collections.reverseOrder());
-
-        System.out.println("Result of ArrayList in Decending order is: ");
+        //show user the new arraylist after sorting in reverse order
+        System.out.println("Result of ArrayList in Descending order is: ");
         for (String Str1 : al) {
             System.out.println(Str1);
         }
