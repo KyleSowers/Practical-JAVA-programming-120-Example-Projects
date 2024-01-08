2068d5032c1faf642d4a36313f52f9207d907bcb
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-07 20:04:39 -0600

    Added line comments to explain code.

--- RemoveDuplicateElements/DuplicateElements.java
+++ RemoveDuplicateElements/DuplicateElements.java
@@ -7,9 +7,9 @@ import java.util.List;
 
 public class DuplicateElements {
     public static void main(String[] args) {
-
+        //create a new arraylist of strings
         List<String> al = new ArrayList<String>();
-
+        //add strings
         al.add("TTT");
         al.add("SSS");
         al.add("TTT");
@@ -17,20 +17,20 @@ public class DuplicateElements {
         al.add("AAA");
         al.add("SSS");
         al.add("TTT");
-
+        //print list before removing duplicates
         System.out.println("List before removing duplicate elements: ");
         System.out.println(al);
-
+        //create new hashset to remove duplicates
         LinkedHashSet<String> hset = new LinkedHashSet<String>();
-
+        //add values in arraylist to hashset
         hset.addAll(al);
-
+        //clear arraylist
         al.clear();
-
+        //add hashed values back into empty arraylist
         al.addAll(hset);
-
+        //create new line
         System.out.println("\n");
-
+        //print new arraylist without doubles
         System.out.println("List after removing duplicate elements: " + al);
     }
 }
