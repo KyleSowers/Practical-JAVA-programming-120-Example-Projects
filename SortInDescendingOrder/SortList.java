2257b86b6118e20beee2c36ec5f4d77c5c6ac4fa
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-07 19:36:46 -0600

    Created a new arraylist in main method of class "SortList".

--- SortInDescendingOrder/SortList.java
+++ SortInDescendingOrder/SortList.java
@@ -1,8 +1,11 @@
 //PRACTICE 90: A PROGRAM TO SORT ARRAY IN DESCENDING ORDER
 package SortInDescendingOrder;
 
+import java.util.ArrayList;
+
 public class SortList {
     public static void main(String[] args) {
 
+        ArrayList<String> al = new ArrayList<String>();
     }
 }
