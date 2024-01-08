e73784b9e8542f6430cb6f7bd2fc48709a0a520f
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-07 19:53:42 -0600

    Created a new string arraylist for class "DuplicateElements".

--- RemoveDuplicateElements/DuplicateElements.java
+++ RemoveDuplicateElements/DuplicateElements.java
@@ -1,8 +1,12 @@
 //PRACTICE 91: A PROGRAM TO REMOVE DUPLICATE ELEMENT IN A LIST
 package RemoveDuplicateElements;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DuplicateElements {
     public static void main(String[] args) {
 
+        List<String> al = new ArrayList<String>();
     }
 }
