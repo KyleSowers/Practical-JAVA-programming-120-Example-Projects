d77a8cfee0e070577266fa9e8f688d6349d837a8
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-08 22:38:24 -0600

    Added line comments.

--- ThreadInJava/UseThread.java
+++ ThreadInJava/UseThread.java
@@ -2,14 +2,15 @@
 package ThreadInJava;
 
 public class UseThread extends Thread {
-
+    //initialize the run method with print statement
     public void run() {
         System.out.println("This is a runnable thread...");
     }
-
+    //create main method
     public static void main(String[] args) {
-
+        //new instance of UseThread
         UseThread ust = new UseThread();
+        //use of start method invokes the run method
         ust.start();
     }
 }
