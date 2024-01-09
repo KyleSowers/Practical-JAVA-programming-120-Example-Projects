67a20f38e767d7bce9678a9d62496f32a16ebac9
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-09 06:52:53 -0600

    Added line comments.

--- RemoveMultipleSpaces/MultipleSpaces.java
+++ RemoveMultipleSpaces/MultipleSpaces.java
@@ -5,14 +5,18 @@ import java.util.StringTokenizer;
 
 public class MultipleSpaces {
     public static void main(String[] args) {
-
+        //create string attribute with value having too many spaces
         String Str1 = "Hello    World   and Today    is java";
+        //tokenizer identifies spaces of Str1 and created attribute Str2
         StringTokenizer Str2 = new StringTokenizer(Str1, " ");
+        //Buffer is mutable unlike String, creating new attribute
         StringBuffer Str3 = new StringBuffer();
-
+        //check for element through loop conditional
         while(Str2.hasMoreElements()) {
+            //add elements to Str3 from Str2, add spaces after
             Str3.append(Str2.nextElement()).append(" ");
         }
+        //print new Str3 and trim out the spaces
         System.out.println(Str3.toString().trim());
     }
 }
