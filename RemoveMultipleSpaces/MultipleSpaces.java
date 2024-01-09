844527c9669ace6ab07e23b33379d546c8744389
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-09 06:40:08 -0600

    Imported and used StringTokenizer to create a new String attribute "Str2" that uses StringTokenizer as a delimeter of " " within Str1.

--- RemoveMultipleSpaces/MultipleSpaces.java
+++ RemoveMultipleSpaces/MultipleSpaces.java
@@ -1,9 +1,12 @@
 // PRACTICE 95: A PROGRAM TO REMOVE MULTIPLE SPACES IN A STRING
 package RemoveMultipleSpaces;
 
+import java.util.StringTokenizer;
+
 public class MultipleSpaces {
     public static void main(String[] args) {
 
         String Str1 = "Hello    World   and Today    is java";
+        StringTokenizer Str2 = new StringTokenizer(Str1, " ");
     }
 }
