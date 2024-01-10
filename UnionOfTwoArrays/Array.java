647a7bde0f82222dbd7dbf9c1d60ffa92494a1bc
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-10 08:29:19 -0600

    Inside of method sumElement, int array "output" is created and stores value that is a copy of num1 and num1 length +1.

--- UnionOfTwoArrays/Array.java
+++ UnionOfTwoArrays/Array.java
@@ -1,9 +1,11 @@
 //PRACTICE 96: A PROGRAM TO FIND THE UNION OF TWO ARRAYS
 package UnionOfTwoArrays;
 
+import java.util.Arrays;
+
 public class Array {
     public static int[] sumElement(int[] num1, int sumed) {
-
+        int[] output = Arrays.copyOf(num1, num1.length+1);
     }
     public static void main(String[] args) {
 
