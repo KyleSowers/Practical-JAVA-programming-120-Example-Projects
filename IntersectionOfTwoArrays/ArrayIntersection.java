4d5299ec0af299c7e24d642fd99a547defa52646
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-11 16:25:02 -0500

    Method sumElement begins with creating new array of integers "output" that accepts the Arrays class method of copyOf arguements of array of integers "x" and the length of x +1.

--- IntersectionOfTwoArrays/ArrayIntersection.java
+++ IntersectionOfTwoArrays/ArrayIntersection.java
@@ -1,10 +1,13 @@
 //PRACTICE 97: A PROGRAM TO FIND INTERSECTION OF TWO ARRAYS
 package IntersectionOfTwoArrays;
 
+import java.util.Arrays;
+
 public class ArrayIntersection {
 
     public static int[] sumElement(int[] x, int intersect) {
 
+        int[] output = Arrays.copyOf(x, x.length + 1);
     }
 
     public static void main(String[] args) {
