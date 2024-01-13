0eca4eef145a7a303b54af5a313969caafedf1de
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-12 20:26:04 -0500

    Added line comments.

--- PowerOfTwo/Power.java
+++ PowerOfTwo/Power.java
@@ -5,11 +5,13 @@ import java.util.Scanner;
 
 public class Power {
     public static void main(String[] args) {
-
+        //ask user for a number
         System.out.println("Please, insert a number: ");
+        //create new scanner
         Scanner sc = new Scanner(System.in);
-
+        //use scanner to store user value in new attribute
         int num1 = sc.nextInt();
+        //through if-else, check condition for power of 2 and print result
         if ((num1 & num1 - 1) == 0) {
             System.out.println("The number " + num1 + " is a power of 2");
         } else {
