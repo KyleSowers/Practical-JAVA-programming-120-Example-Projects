82930ac7c0e9a1cd375feb8f1cef50aa2548804e
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-12 20:22:57 -0500

    An if satement is added that checks result of the bitwise AND operator to be equal to one, which commonly means the number is a power of 2, and if met a print statement prints the result that the number is a power of 2.

--- PowerOfTwo/Power.java
+++ PowerOfTwo/Power.java
@@ -10,5 +10,8 @@ public class Power {
         Scanner sc = new Scanner(System.in);
 
         int num1 = sc.nextInt();
+        if ((num1 & num1 - 1) == 0) {
+            System.out.println("The number " + num1 + " is a power of 2");
+        }
     }
 }
