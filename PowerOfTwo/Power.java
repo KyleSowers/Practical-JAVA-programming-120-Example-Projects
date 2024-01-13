80264447efc6617b79fdbe97743fcf8eb89f5929
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-12 20:24:26 -0500

    An else statement is added following the if statement to print the case of it not being a number that is the power of two if the if statment condition was not satisfied.

--- PowerOfTwo/Power.java
+++ PowerOfTwo/Power.java
@@ -12,6 +12,8 @@ public class Power {
         int num1 = sc.nextInt();
         if ((num1 & num1 - 1) == 0) {
             System.out.println("The number " + num1 + " is a power of 2");
+        } else {
+            System.out.println("The number " + num1 + " is not a power of 2");
         }
     }
 }
