fd898b36004a8f47fb10ca7896c8487c564b42a9
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-12 20:38:00 -0500

    Added line comments.

--- ExceptionHandlingInJava/ExceptionHandling.java
+++ ExceptionHandlingInJava/ExceptionHandling.java
@@ -4,13 +4,14 @@ package ExceptionHandlingInJava;
 public class ExceptionHandling {
 
     public static void main(String[] args) {
-
+        //declare attributes with values
         int a =30;
         int b = 0;
-
+        //try statement, with arithmatic
         try {
             float c = a/b;
             System.out.println("The result of C is : " + c);
+            //catch statment that catches the arithmatic error of use of  0 in multiplication
         } catch (ArithmeticException e) {
             System.out.println("Please do not use 0 as a divider");
         }
