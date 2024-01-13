d0953c03cc4223233955183f67f5bfbd46023e2b
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-12 20:14:57 -0500

    A new scanner is created "sc" in the main method in order to read the user input from the previous print statement. The Scanner class was imported.

--- PowerOfTwo/Power.java
+++ PowerOfTwo/Power.java
@@ -1,10 +1,12 @@
 //PRACTICE 98: A PROGRAM TO DETECT IF A NUMBER IS OF POWER OF 2
 package PowerOfTwo;
 
+import java.util.Scanner;
+
 public class Power {
     public static void main(String[] args) {
 
         System.out.println("Please, insert a number: ");
-
+        Scanner sc = new Scanner(System.in);
     }
 }
