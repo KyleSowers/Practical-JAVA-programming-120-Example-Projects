985ed037b4bb19d37e46ea8d191436ee94ec068a
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-13 22:12:13 -0500

    Added line comments.

--- ExceptionHandlingUsingFinally/ExceptionHandling.java
+++ ExceptionHandlingUsingFinally/ExceptionHandling.java
@@ -2,16 +2,18 @@
 package ExceptionHandlingUsingFinally;
 
 public class ExceptionHandling {
-
+    //create method
     public String FinallyBlock() {
+        //method try block
         try {
             return "Return from try block";
+            //method finally block will execute no matter result of try block
         } finally {
             System.out.println("Return from finally block after return statement");
         }
     }
     public static void main(String[] args) {
-
+        //call for class method to print
         System.out.println(new ExceptionHandling().FinallyBlock());
     }
 }
