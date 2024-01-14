8cc68adad9bcc263c26c7335e5ee0a0f97441f59
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-13 21:54:59 -0500

    Added line comments.

--- MultipleCatchBlocks/ExceptionHandling.java
+++ MultipleCatchBlocks/ExceptionHandling.java
@@ -5,12 +5,13 @@ import IntersectionOfTwoArrays.ArrayIntersection;
 
 public class ExceptionHandling {
     public static void main(String[] args) {
-
+        //create try statement with body
         try {
             int x[] = new int[10];
             x[5] = 4/8;
 
 //            System.out.println(y);
+            //create catch error conditionals
         } catch (ArithmeticException e) {
             System.out.println("This is an " + e);
         } catch (ArrayIndexOutOfBoundsException e) {
@@ -18,6 +19,7 @@ public class ExceptionHandling {
         } catch (Exception e) {
             System.out.println("This is an " + e);
         }
+        //print advising the try-catches has been completed
         System.out.println("Out of try-catch block");
     }
 }
