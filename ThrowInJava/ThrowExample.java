b55aa2dbf4e58c6e36ed9c77306d8a9fe0229bb3
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-14 10:45:56 -0500

    Added the else to the if-else statement that simply prints the statment that the yr is a leap year because it did not pass the conditional check of the leap year for not being a leap year.

--- ThrowInJava/ThrowExample.java
+++ ThrowInJava/ThrowExample.java
@@ -6,6 +6,8 @@ public class ThrowExample {
     public static void LeapYear(int yr) {
         if (yr % 4 != 0) {
             throw new ArithmeticException("This is not a leap year");
+        } else {
+            System.out.println("This is a leap year");
         }
     }
 }
