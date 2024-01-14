4b3b2b741bd7b04eb330a7f5d7b61159fb41cf66
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-14 10:53:44 -0500

    Added line comments.

--- ThrowInJava/ThrowExample.java
+++ ThrowInJava/ThrowExample.java
@@ -2,18 +2,22 @@
 package ThrowInJava;
 
 public class ThrowExample {
-
+    //create method for determining leap year
     public static void LeapYear(int yr) {
+        //check if condition of not being a leap year is met
         if (yr % 4 != 0) {
+            //throw exception if condition of if was satisfied, inform user not a leap year
             throw new ArithmeticException("This is not a leap year");
+            //if condition not met, than leap year, inform user
         } else {
             System.out.println("This is a leap year");
         }
     }
 
     public static void main(String[] args) {
-
+        //tell user check for leap year is beginning
         System.out.println("Checking leap year");
+        //call method with arguement value to check for leap year and provide result to user
         LeapYear(2023);
     }
 }
