5783e86782520cfefbb06ed14b7eab4f499377a3
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-13 21:46:51 -0500

    The first catch added following the try statement, which has the conditional for ArithmaticException, which would print statement advising error.

--- MultipleCatchBlocks/ExceptionHandling.java
+++ MultipleCatchBlocks/ExceptionHandling.java
@@ -6,6 +6,10 @@ public class ExceptionHandling {
         try {
             int x[] = new int[10];
             x[5] = 4/8;
+
+            System.out.println(y);
+        } catch (ArithmeticException e) {
+            System.out.println("This is an " + e);
         }
     }
 }
