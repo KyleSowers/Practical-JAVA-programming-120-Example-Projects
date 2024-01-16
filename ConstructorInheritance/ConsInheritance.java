1d508b2c0c767de84b848a086aea49b80a331bf4
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-15 23:59:44 -0500

    Added line comments.

--- ConstructorInheritance/ConsInheritance.java
+++ ConstructorInheritance/ConsInheritance.java
@@ -2,24 +2,25 @@
 package ConstructorInheritance;
 
 public class ConsInheritance {
+    //new class with constructor
     static class student {
         student() {
             System.out.println("This is first student constructor");
         }
     }
-
+    //new extended class with constructor
     static class school extends student {
         school() {
             System.out.println("This is second student constructor");
         }
     }
-
+    //new extended class with constructor
     static class studentschool extends school {
         studentschool() {
             System.out.println("This is third student constructor");
         }
     }
-
+    //call on method that extends previous classes providing each constructor result
     public static void main(String[] args) {
         studentschool ss = new studentschool();
     }
