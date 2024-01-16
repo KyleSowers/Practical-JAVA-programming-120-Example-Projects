4b50137e724f8abd3b911eb2d2177798183a0f7f
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-15 23:58:02 -0500

    Made classes static to allow main method to resolve from error and run.

--- ConstructorInheritance/ConsInheritance.java
+++ ConstructorInheritance/ConsInheritance.java
@@ -2,25 +2,25 @@
 package ConstructorInheritance;
 
 public class ConsInheritance {
-    class student {
+    static class student {
         student() {
             System.out.println("This is first student constructor");
         }
     }
 
-    class school extends student {
+    static class school extends student {
         school() {
             System.out.println("This is second student constructor");
         }
     }
 
-    class studentschool extends school {
+    static class studentschool extends school {
         studentschool() {
             System.out.println("This is third student constructor");
         }
     }
 
-    public static void  main(String[] args) {
+    public static void main(String[] args) {
         studentschool ss = new studentschool();
     }
 }
