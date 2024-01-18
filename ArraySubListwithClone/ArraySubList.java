2df8420b7d193789ec5057c3263d8332c13d18b3
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-17 20:43:54 -0500

    Added nine different strings to alist using the add method.

--- ArraySubListwithClone/ArraySubList.java
+++ ArraySubListwithClone/ArraySubList.java
@@ -8,6 +8,14 @@ public class ArraySubList {
 
         ArrayList<String> alist = new ArrayList<String>();
 
-
+        alist.add("111");
+        alist.add("222");
+        alist.add("333");
+        alist.add("444");
+        alist.add("555");
+        alist.add("666");
+        alist.add("777");
+        alist.add("888");
+        alist.add("999");
     }
 }
