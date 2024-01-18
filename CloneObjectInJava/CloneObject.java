39210f2d21eac5f6f598b3d6c2b997a375c1d1d4
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-17 20:37:30 -0500

    Added line comments.

--- CloneObjectInJava/CloneObject.java
+++ CloneObjectInJava/CloneObject.java
@@ -2,21 +2,23 @@
 package CloneObjectInJava;
 
 public class CloneObject implements Cloneable{
+    //create attributes
     String name;
     int record;
-
+    //constructor for attributes
     public CloneObject(String name, int record) {
         this.name = name;
         this.record = record;
     }
-
+    //method to show result
     public void show() {
         System.out.println("Welcome " + name + "! Your score is: " + record);
     }
 
     public static void main(String[] args) {
-
+        //create new object
         CloneObject co = new CloneObject("David", 98);
+        //try-catch to attempt to clone or catch clone not supported exception
         try {
             CloneObject co1 = (CloneObject)co.clone();
             co1.show();
