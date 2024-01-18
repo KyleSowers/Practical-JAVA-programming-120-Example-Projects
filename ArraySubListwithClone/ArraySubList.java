46528780aed773da2c651d9d7eadfffdf6608a5a
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-17 20:42:25 -0500

    Created a new ArrayList of Strings.

--- ArraySubListwithClone/ArraySubList.java
+++ ArraySubListwithClone/ArraySubList.java
@@ -1,8 +1,13 @@
 //PRACTICE 108: A PROGRAM TO GET SUB-LIST FROM AN ARRAY WITH CLONE
 package ArraySubListwithClone;
 
+import java.util.ArrayList;
+
 public class ArraySubList {
     public static void main(String args[]) {
+
+        ArrayList<String> alist = new ArrayList<String>();
+
 
     }
 }
