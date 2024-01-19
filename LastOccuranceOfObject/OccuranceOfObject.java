70056ea125abf64609096aa025302bfa77e92856
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-18 20:48:38 -0500

    Added 16 lines of list1 using the add method to add string element values to list1.

--- LastOccuranceOfObject/OccuranceOfObject.java
+++ LastOccuranceOfObject/OccuranceOfObject.java
@@ -7,5 +7,22 @@ import java.util.List;
 public class OccuranceOfObject {
     public static void  main(String[] args) {
         List<String> list1 = new ArrayList<String>();
+
+        list1.add("000");
+        list1.add("222");
+        list1.add("444");
+        list1.add("333");
+        list1.add("222");
+        list1.add("000");
+        list1.add("111");
+        list1.add("555");
+        list1.add("888");
+        list1.add("444");
+        list1.add("333");
+        list1.add("000");
+        list1.add("111");
+        list1.add("222");
+        list1.add("444");
+        list1.add("555");
     }
 }
