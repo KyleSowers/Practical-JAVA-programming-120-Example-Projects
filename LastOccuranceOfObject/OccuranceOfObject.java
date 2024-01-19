ac063a385c21b20d9528288f67f03b1821582597
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-18 21:16:44 -0500

    New print statment advises user of last "444" in list1.

--- LastOccuranceOfObject/OccuranceOfObject.java
+++ LastOccuranceOfObject/OccuranceOfObject.java
@@ -31,6 +31,7 @@ public class OccuranceOfObject {
         System.out.println("last occurrence of 111 is: " + list1.lastIndexOf("111"));
         System.out.println("last occurrence of 222 is: " + list1.lastIndexOf("222"));
         System.out.println("last occurrence of 333 is: " + list1.lastIndexOf("333"));
+        System.out.println("last occurrence of 444 is: " + list1.lastIndexOf("444"));
 
     }
 }
