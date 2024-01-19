2d6f53cd39cb9c6b427a1ecd1d8d6be8725f66a3
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-18 20:22:47 -0500

    added the nine string values to be added to the new list.

--- SynchronizeArrayList/SyncArray.java
+++ SynchronizeArrayList/SyncArray.java
@@ -9,14 +9,14 @@ public class SyncArray implements  Cloneable {
     public static void main(String[] args) {
         List<String> list = Collections.synchronizedList(new ArrayList<String>());
 
-        list.add("");
-        list.add("");
-        list.add("");
-        list.add("");
-        list.add("");
-        list.add("");
-        list.add("");
-        list.add("");
-        list.add("");
+        list.add("111");
+        list.add("222");
+        list.add("333");
+        list.add("444");
+        list.add("555");
+        list.add("666");
+        list.add("777");
+        list.add("888");
+        list.add("999");
     }
 }
