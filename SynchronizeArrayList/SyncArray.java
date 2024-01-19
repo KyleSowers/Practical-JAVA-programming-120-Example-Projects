e83dbcfef2014749f705e4cf9c57a5b8674af961
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-18 20:20:20 -0500

    Created new List for strings that calls on Collections synchronizedList.

--- SynchronizeArrayList/SyncArray.java
+++ SynchronizeArrayList/SyncArray.java
@@ -1,8 +1,12 @@
 //PRACTICE 109: A PROGRAM TO SYNC ARRAY LIST USING COLLECTIONS.SYNCHOCHRONISELIST
 package SynchronizeArrayList;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class SyncArray implements  Cloneable {
     public static void main(String[] args) {
-
+        List<String> list = Collections.synchronizedList(new ArrayList<String>());
     }
 }
