8c5f8d4dde0ee4ab974008571302192d5044b87d
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-18 20:46:14 -0500

    Imported List from java.utils, created new ArrayList for strings.

--- LastOccuranceOfObject/OccuranceOfObject.java
+++ LastOccuranceOfObject/OccuranceOfObject.java
@@ -1,8 +1,11 @@
 //PRACTICE 110: A PROGRAM TO GET THE LAST OCCURRENCE OF AN OBJECT IN AN ARRAY LIST
 package LastOccuranceOfObject;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class OccuranceOfObject {
     public static void  main(String[] args) {
-
+        List<String> list1 = new ArrayList<String>();
     }
 }
