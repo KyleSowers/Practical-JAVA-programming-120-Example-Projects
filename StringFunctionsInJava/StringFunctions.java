c6e2fb854e0eaf23177a6a87bf7b0b32cb9da135
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-22 08:34:18 -0500

    Added a print statement that advises what str4 is.

--- StringFunctionsInJava/StringFunctions.java
+++ StringFunctionsInJava/StringFunctions.java
@@ -13,5 +13,6 @@ public class StringFunctions {
         System.out.println("Str1 is: " + str1);
         System.out.println("Str2 is: " + str2);
         System.out.println("Str3 is: " + str3);
+        System.out.println("Str4 is: " + str4);
     }
 }
