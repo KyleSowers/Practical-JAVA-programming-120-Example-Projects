1f55281df676a401f9422f316c7167e4a80d2cfd
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-23 18:50:59 -0500

    Added line comments for code.

--- CountNumberOfWords/CountWords.java
+++ CountNumberOfWords/CountWords.java
@@ -4,8 +4,9 @@ package CountNumberOfWords;
 import java.util.Scanner;
 
 public class CountWords {
-
+    //declare variables/attributes
     static int z, y = 0, output;
+    //method for solving number f words
     static int count(String s) {
         char ch[] = new char[s.length()];
         for (z = 0; z < s.length(); z++) {
@@ -16,13 +17,19 @@ public class CountWords {
         }
         return y;
     }
-
+    //main method for execution
     public static void main(String args[]) {
+        //declare attribute to be checked as empty
         String Str1 = " ";
+        //create scanner for user input
         Scanner sc = new Scanner(System.in);
+        //request word(s)
         System.out.println("Please, insert some word");
+        //store user value into empty attribute
         Str1 = sc.nextLine();
+        //declare new attribute that calls on method from class to solve for count of words
         int words = CountWords.count(Str1);
+        //inform user how many words
         System.out.println("the number of words is: " + words);
     }
 }
