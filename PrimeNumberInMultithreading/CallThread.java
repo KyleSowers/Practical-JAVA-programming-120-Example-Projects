9af9671f1320e0ebff225c9d810d39edc1edd1a8
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-25 20:39:33 -0500

    Before the for loop, attribute/field "res" is given value of false.

--- PrimeNumberInMultithreading/CallThread.java
+++ PrimeNumberInMultithreading/CallThread.java
@@ -8,6 +8,7 @@ class GetPrime extends Thread {
         int num1, num2 = 2;
         boolean res;
 
+        res = false;
         for (num2 = 2; num2 <= 100; num2++) {
 
             if (num2 % num1 == 0) {
