ba7fe7530ce8c4eb93553ebe6bfd5bd33047350e
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-25 20:20:33 -0500

    Added a new class "GetPrime".

--- PrimeNumberInMultithreading/CallThread.java
+++ PrimeNumberInMultithreading/CallThread.java
@@ -1,6 +1,10 @@
 //PRACTICE 117: A PROGRAM TO FIND PRIME NUMBERS USING MULTITHREADING
 package PrimeNumberInMultithreading;
 
+class GetPrime {
+
+}
+
 public class CallThread {
     public static void main(String[] args) {
 
