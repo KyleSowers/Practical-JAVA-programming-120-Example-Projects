efa2fee832848a011622d6946164d589a6c622ea
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-25 20:47:04 -0500

    Added a required for loop for complete factoring for prime number, due to run time error.

--- PrimeNumberInMultithreading/CallThread.java
+++ PrimeNumberInMultithreading/CallThread.java
@@ -8,17 +8,20 @@ class GetPrime extends Thread {
         int num1, num2 = 2;
         boolean res;
 
-        res = false;
-        for (num1 = 2; num2 <= 100; num2++) {
+        for (num2 = 2; num2 <= 100; num2++) {
 
-            if (num2 % num1 == 0) {
+            res = false;
+            for (num1 = 2; num1 < num2; num1++) {
 
-                res = true;
-                break;
+                if (num2 % num1 == 0) {
+
+                    res = true;
+                    break;
+                }
             }
-        }
-        if (res == false) {
-            System.out.println(num2 + " is a prime number");
+            if (res == false) {
+                System.out.println(num2 + " is a prime number");
+            }
         }
     }
 }
