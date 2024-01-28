5d46ec27b50d66dc6baef7c9b98a9769398ac4a6
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-27 20:14:52 -0500

    Created a new class "LowerAZ" that will serve the purpose for method factoring.

--- PrintAlphabetAlternate/AlphabetAZ.java
+++ PrintAlphabetAlternate/AlphabetAZ.java
@@ -1,6 +1,10 @@
 //PRACTICE 119: A PROGRAM TO PRINT OUT ALPHABET USING ALTERNATE
 package PrintAlphabetAlternate;
 
+class LowerAZ {
+
+}
+
 public class AlphabetAZ {
 
     public static void main(String[] args) {
