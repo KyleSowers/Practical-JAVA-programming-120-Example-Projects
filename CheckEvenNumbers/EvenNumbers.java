f522eaa0b9c6208d8f897774c9302c5301ca6dcf
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-29 08:24:41 -0500

    Added line comments.

--- CheckEvenNumbers/EvenNumbers.java
+++ CheckEvenNumbers/EvenNumbers.java
@@ -1,10 +1,12 @@
 //PRACTICE 121: A PROGRAM TO CHECK FOR EVEN NUMBERS
 package CheckEvenNumbers;
-
+//class for method to solve for even numbers
 class Even extends Thread {
-
+    //method for solving
     public void run() {
+        //iterate starting at value 1, post incrementing through and up to 10
         for (int x = 1; x <= 10; x++) {
+            //determine if number is even and print result if or if not
             if (x % 2 == 0) {
                 System.out.println(x + " is an even number");
             } else {
@@ -15,10 +17,11 @@ class Even extends Thread {
 }
 
 public class EvenNumbers {
-
+    //main class
     public static void main(String[] args) {
-
+        //new instance/object of class Even
         Even e = new Even();
+        //use instance, through extended thread, to invoke the "Even" class method run()
         e.start();
     }
 }
