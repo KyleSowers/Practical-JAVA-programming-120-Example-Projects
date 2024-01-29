016f9a017a2e2ff0a325e27e96e4f6d8ac289a8c
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-28 22:10:53 -0500

    In the main method, a new instance/object of class "printNumbers" is created.

--- PrintNumber1to10/CallNumbers.java
+++ PrintNumber1to10/CallNumbers.java
@@ -1,7 +1,7 @@
 //PRACTICE 120: A PROGRAM TO PRINT OUT NUMBERS
 package PrintNumber1to10;
 
-class PrintNumbers extends Thread {
+class printNumbers extends Thread {
 
     public void run() {
         for (int x = 1; x <= 10; x++) {
@@ -21,6 +21,6 @@ class PrintNumbers extends Thread {
 public class CallNumbers {
 
     public static void main(String[] args) {
-
+         printNumbers pn = new printNumbers();
     }
 }
