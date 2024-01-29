fb584ca8312556b28104f823f303fd9e22d98899
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-28 22:15:10 -0500

    Added line comments.

--- PrintNumber1to10/CallNumbers.java
+++ PrintNumber1to10/CallNumbers.java
@@ -1,12 +1,15 @@
 //PRACTICE 120: A PROGRAM TO PRINT OUT NUMBERS
 package PrintNumber1to10;
 
+//create class for method to print out numbers
 class printNumbers extends Thread {
-
+    //method
     public void run() {
+        //print out on same line
         for (int x = 1; x <= 10; x++) {
             System.out.print(" " + x);
         }
+        //printing results on new lines per iteration
         for (int x = 1; x <= 10; x++) {
             try {
                 System.out.println(" " + x);
@@ -19,9 +22,11 @@ class printNumbers extends Thread {
 }
 
 public class CallNumbers {
-
+    //main method
     public static void main(String[] args) {
+        //create new instance/object of printNumbers class
          printNumbers pn = new printNumbers();
+         //use new object and Thread to run the run() method
          pn.start();
     }
 }
