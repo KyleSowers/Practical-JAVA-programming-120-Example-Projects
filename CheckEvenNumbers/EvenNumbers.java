edd05d4d94a657cca2a3f69775b4db356a8059b5
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-29 08:16:05 -0500

    If the condition of the for loop x <= 10 is satisfied and the condition of the nested if statement x % 2 == 0, added the body of the if statement that will print out that x is an even number.

--- CheckEvenNumbers/EvenNumbers.java
+++ CheckEvenNumbers/EvenNumbers.java
@@ -6,7 +6,7 @@ class Even extends Thread {
     public void run() {
         for (int x = 1; x <= 10; x++) {
             if (x % 2 == 0) {
-
+                System.out.println(x + " is an even number");
             }
         }
     }
