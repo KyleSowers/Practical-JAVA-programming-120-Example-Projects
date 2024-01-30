9f45e9de4d78692c91f2aa2b3d9459e0b15d4996
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-30 08:47:10 -0500

    Added line comments explaining code.

--- CheckOddNumbers/OddNumbers.java
+++ CheckOddNumbers/OddNumbers.java
@@ -1,13 +1,17 @@
 //PRACTICE 122: A PROGRAM TO CHECK FOR ODD NUMBERS
 package CheckOddNumbers;
-
+//class for desired method using threading
 class Odd extends Thread {
-
+    //method to check for odd numbers
     public void run() {
+        //for loop declares attribute with value, checks condition, post increments until condition is no loger satisfied
         for (int x = 1; x <= 10; x++) {
+            //if for loop condition satisfied, nested if statement divides attribute by two checking there is a remainder value
             if (x % 2 !=0) {
+                //prints if there was a remainder value
                 System.out.println(x + " is an odd number");
             } else {
+                //prints if there was not a remainder value
                 System.out.println(x + " is not an odd number");
             }
         }
@@ -15,10 +19,11 @@ class Odd extends Thread {
 }
 
 public class OddNumbers {
-
+    //declare main method
     public static void main(String[] args) {
-
+        //create new instance of class "Odd"
         Odd o = new Odd();
+        //use instance to call start() method, through extended thread will run class "Odd"s method run()
         o.start();
     }
 }
