4509dbc2abd40eb303c30ba48d0287c5fef79068
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-30 08:34:27 -0500

    Inside the run() method, after satisfying the for loop condition of x <= 10 and the nested if statement leaving a remainer to x after divided by 2, added a print statement that x is in fact an odd number for satisfying the for and if condition.

--- CheckOddNumbers/OddNumbers.java
+++ CheckOddNumbers/OddNumbers.java
@@ -6,7 +6,7 @@ class Odd extends Thread {
     public void run() {
         for (int x = 1; x <= 10; x++) {
             if (x % 2 !=0) {
-
+                System.out.println(x + " is an odd number");
             }
         }
     }
