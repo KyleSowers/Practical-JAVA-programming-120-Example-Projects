d3b56e053d298e7fb3e444ea030604557b59171d
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-31 20:47:08 -0500

    Inside class "AdditionSubtraction" before the main method, imported the javax.swing.* and declared a new instance of JFrame "frame".

--- GetSquareOfNumbers/AdditionSubtraction.java
+++ GetSquareOfNumbers/AdditionSubtraction.java
@@ -1,7 +1,12 @@
 //PRACTICE 123: A SWING PROGRAM FOR ADDITION AND SUBTRACTION
 package GetSquareOfNumbers;
 
+import javax.swing.*;
+
 public class AdditionSubtraction {
+
+    private JFrame frame;
+
     public static void main(String[] args) {
 
     }
