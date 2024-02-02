c93b41ccaf7025d515aaff1290aec1c5fffe4f4f
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-02-01 20:38:23 -0500

    Made txtDisplay a new instance of JTextField.

--- GetSquareOfNumbers/AdditionSubtraction.java
+++ GetSquareOfNumbers/AdditionSubtraction.java
@@ -93,5 +93,7 @@ public class AdditionSubtraction {
         btnSub.setFont(new Font("Tahoma", Font.BOLD, 15));
         btnSub.setBounds(329, 171, 108, 51);
         frame.getContentPane().add(btnSub);
+
+        txtDisplay = new JTextField();
     }
 }
