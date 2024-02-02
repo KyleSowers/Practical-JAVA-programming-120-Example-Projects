e62dd4ca4c0804412443ade9098aeb25ddefeb3d
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-02-01 20:11:27 -0500

    After the values for num1 and num2 are stored, added ans accepting the value of num1 plus num2.

--- GetSquareOfNumbers/AdditionSubtraction.java
+++ GetSquareOfNumbers/AdditionSubtraction.java
@@ -57,7 +57,9 @@ public class AdditionSubtraction {
                 try {
                     num1 = Integer.parseInt(addvalue.getText());
 
-                    num2 = Integer.parseint(subvalue.getText());
+                    num2 = Integer.parseInt(subvalue.getText());
+
+                    ans = num1 + num2;
                 }
             }
         });
